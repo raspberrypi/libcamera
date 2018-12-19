7e9e508093bfa22635155352e73a31efdf457419
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2018-12-19 11:45:45 +0200

    libcamera: Remove libcamera class
    The class was just a placeholder, now that we have other objects
    defined, remove it along with the associated test.
    
    The libcamera/libcamera.h header is kept as a shortcut to include the
    whole libcamera public API.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- include/libcamera/libcamera.h
+++ include/libcamera/libcamera.h
@@ -10,14 +10,4 @@
 #include <libcamera/camera.h>
 #include <libcamera/camera_manager.h>
 
-namespace libcamera {
-
-class libcamera
-{
-public:
-	void init_lib(void);
-};
-
-};
-
 #endif /* __LIBCAMERA_LIBCAMERA_H__ */

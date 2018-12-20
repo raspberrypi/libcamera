20d15d9dc47fcfbf1f02689cc4c882115abe507f
Niklas Söderlund <niklas.soderlund@ragnatech.se>
2018-12-20 16:41:50 +0100

    tests: add test to list all cameras in the system
    Add simple test which lists all cameras detected in the system. The test
    fails if no camera can be found.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>

---
+++ test/list.cpp
@@ -0,0 +1,54 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (C) 2018, Google Inc.
+ *
+ * list.cpp - camera list tests
+ */
+
+#include <iostream>
+
+#include <libcamera/camera_manager.h>
+
+#include "test.h"
+
+using namespace std;
+using namespace libcamera;
+
+class ListTest : public Test
+{
+protected:
+	int init()
+	{
+		cm = new CameraManager();
+		if (!cm)
+			return -ENOMEM;
+
+		cm->start();
+
+		return 0;
+	}
+
+	int run()
+	{
+		unsigned int count = 0;
+
+		for (auto name : cm->list()) {
+			cout << "- " << name << endl;
+			count++;
+		}
+
+		return count ? 0 : -ENODEV;
+	}
+
+	void cleanup()
+	{
+		cm->stop();
+
+		delete cm;
+	}
+
+private:
+	CameraManager *cm;
+};
+
+TEST_REGISTER(ListTest)

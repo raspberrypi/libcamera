raspberrypi/libcamera

commit edbd2059d8a4bd759302ada4368fa4055638fd7f

libcamera: Add initial logger
The logger is based on the ostream API, allowing code to log messages in
a native way. It automatically logs the time stamp, severity level, file
name and line number.

Many important features are missing, such as logging to file, logging
classes, and log filtering based on the severity level, file name and
class.

Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Acked-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/libcamera/include/log.h
@@ -1,0 +1,38 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2018, Google Inc.
+ *
+ * log.h - Logging infrastructure
+ */
+#ifndef __LIBCAMERA_LOG_H__
+#define __LIBCAMERA_LOG_H__
+
+#include <sstream>
+
+namespace libcamera {
+
+enum LogSeverity {
+	LogInfo,
+	LogWarning,
+	LogError,
+};
+
+class LogMessage
+{
+public:
+	LogMessage(const char *fileName, unsigned int line,
+		  LogSeverity severity);
+	LogMessage(const LogMessage&) = delete;
+	~LogMessage();
+
+	std::ostream& stream() { return msgStream; }
+
+private:
+	std::ostringstream msgStream;
+};
+
+#define LOG(severity) LogMessage(__FILE__, __LINE__, Log##severity).stream()
+
+};
+
+#endif /* __LIBCAMERA_LOG_H__ */

src/libcamera/log.cpp
@@ -1,0 +1,81 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2018, Google Inc.
+ *
+ * log.h - Logging infrastructure
+ */
+
+#include <cstdio>
+#include <ctime>
+#include <iomanip>
+#include <string.h>
+
+#include "log.h"
+#include "utils.h"
+
+/**
+ * \file log.h
+ * \brief Logging infrastructure
+ */
+
+namespace libcamera {
+
+/**
+ * \enum LogSeverity
+ * Log message severity
+ * \var Info
+ * Informational message
+ * \var Warning
+ * Warning message, signals a potential issue
+ * \var Error
+ * Error message, signals an unrecoverable issue
+ */
+
+/**
+ * \def LOG(severity)
+ * \brief Log a message
+ *
+ * Return an std::ostream reference to which a message can be logged using the
+ * iostream API. The \a severity controls whether the message is printed or
+ * dropped, depending on the global log level.
+ */
+
+static const char *log_severity_name(LogSeverity severity)
+{
+	static const char * const names[] = {
+		"INFO",
+		"WARN",
+		" ERR",
+	};
+
+	if ((unsigned int)severity < ARRAY_SIZE(names))
+		return names[severity];
+	else
+		return "UNKN";
+}
+
+LogMessage::LogMessage(const char *fileName, unsigned int line,
+		       LogSeverity severity)
+{
+	/* Log the timestamp, severity and file information. */
+	struct timespec timestamp;
+	clock_gettime(CLOCK_MONOTONIC, &timestamp);
+	msgStream << "[" << timestamp.tv_sec / (60 * 60) << ":"
+		  << std::setw(2) << (timestamp.tv_sec / 60) % 60 << ":"
+		  << std::setw(2) << timestamp.tv_sec % 60 << "."
+		  << std::setw(9) << timestamp.tv_nsec << "]";
+
+	msgStream << " " << log_severity_name(severity);
+	msgStream << " " << basename(fileName) << ":" << line << " ";
+}
+
+LogMessage::~LogMessage()
+{
+	msgStream << std::endl;
+
+	std::string msg(msgStream.str());
+	fwrite(msg.data(), msg.size(), 1, stderr);
+	fflush(stderr);
+}
+
+};

src/libcamera/include/utils.h
@@ -1,0 +1,12 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2018, Google Inc.
+ *
+ * utils.h - Miscellaneous utility functions
+ */
+#ifndef __LIBCAMERA_UTILS_H__
+#define __LIBCAMERA_UTILS_H__
+
+#define ARRAY_SIZE(a)	(sizeof(a) / sizeof(a[0]))
+
+#endif /* __LIBCAMERA_UTILS_H__ */

src/libcamera/meson.build
@@ -1,4 +1,7 @@
-sources = ['main.cpp']
+sources = files([
+    'log.cpp',
+    'main.cpp',
+])
 
 includes = [
     libcamera_includes,

commit d67841a9906935523bdb2b314e09a041091aa3ca

libcamera: log: Fix coding style warnings
The checkstyle utility highlights some minor warnings against the logger
implementation.

Fix these up.

Fixes: edbd2059d8a4 ("libcamera: Add initial logger")
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/libcamera/include/log.h
@@ -23,11 +23,11 @@ class LogMessage
 {
 public:
 	LogMessage(const char *fileName, unsigned int line,
-		  LogSeverity severity);
-	LogMessage(const LogMessage&) = delete;
+		   LogSeverity severity);
+	LogMessage(const LogMessage &) = delete;
 	~LogMessage();
 
-	std::ostream& stream() { return msgStream; }
+	std::ostream &stream() { return msgStream; }
 
 private:
 	std::ostringstream msgStream;

src/libcamera/log.cpp
@@ -66,7 +66,7 @@ namespace libcamera {
 
 static const char *log_severity_name(LogSeverity severity)
 {
-	static const char * const names[] = {
+	static const char *const names[] = {
 		"  DBG",
 		" INFO",
 		" WARN",
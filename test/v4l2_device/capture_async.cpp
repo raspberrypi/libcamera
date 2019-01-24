ba4dfa74714785005232e4268d63df1a4f162e24
Kieran Bingham <kieran.bingham@ideasonboard.com>
2019-01-24 17:48:10 +0000

    test: v4l2_device: Provide asynchronous capture test
    Utilise the event_dispatcher to create a default event loop, and process
    asynchronous buffer receive events.
    
    If no frames are captured in 5 seconds, the test will fail.
    It will also fail if less than 30 frames have been captured in the same
    timeout period.
    
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
    Signed-off-by: Jacopo Mondi <jacopo@jmondi.org>
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>

---
+++ test/v4l2_device/capture_async.cpp
@@ -0,0 +1,86 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (C) 2019, Google Inc.
+ *
+ * libcamera V4L2 API tests
+ */
+
+#include <libcamera/buffer.h>
+#include <libcamera/camera_manager.h>
+#include <libcamera/event_dispatcher.h>
+#include <libcamera/timer.h>
+
+#include <iostream>
+
+#include "v4l2_device_test.h"
+
+class CaptureAsyncTest : public V4L2DeviceTest
+{
+public:
+	CaptureAsyncTest()
+		: frames(0){};
+
+	void receiveBuffer(Buffer *buffer)
+	{
+		std::cout << "Received buffer " << buffer->index() << std::endl;
+		frames++;
+
+		/* Requeue the buffer for further use. */
+		dev_->queueBuffer(buffer);
+	}
+
+protected:
+	int run()
+	{
+		const unsigned int bufferCount = 8;
+
+		EventDispatcher *dispatcher = CameraManager::instance()->eventDispatcher();
+		Timer timeout;
+		int ret;
+
+		createBuffers(bufferCount);
+
+		ret = dev_->exportBuffers(bufferCount, &pool_);
+		if (ret)
+			return TestFail;
+
+		dev_->bufferReady.connect(this, &CaptureAsyncTest::receiveBuffer);
+
+		/* Queue all the buffers to the device. */
+		for (Buffer &b : pool_.buffers()) {
+			if (dev_->queueBuffer(&b))
+				return TestFail;
+		}
+
+		ret = dev_->streamOn();
+		if (ret)
+			return TestFail;
+
+		timeout.start(5000);
+		while (timeout.isRunning())
+			dispatcher->processEvents();
+
+		if (frames < 1) {
+			std::cout << "Failed to capture any frames within timeout." << std::endl;
+			return TestFail;
+		}
+
+		if (frames < 30) {
+			std::cout << "Failed to capture 30 frames within timeout." << std::endl;
+			return TestFail;
+		}
+
+		std::cout << "Processed " << frames << " frames" << std::endl;
+
+		ret = dev_->streamOff();
+		if (ret)
+			return TestFail;
+
+		return TestPass;
+	}
+
+private:
+	unsigned int frames;
+};
+
+TEST_REGISTER(CaptureAsyncTest);

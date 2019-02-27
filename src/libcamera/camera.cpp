raspberrypi/libcamera

commit 65ea2422d24a30736e457fdefe31956b4428ae0f

libcamera: camera: extend camera object to support configuration of streams
Extend the camera to support reading and configuring formats for
groups of streams. The implementation in the Camera are minimalistic as
the heavy lifting are done by the pipeline handler implementations.

The most important functionality the camera provides in this context is
validation of data structures passed to it from the application and
access control to the pipeline handler.

Signed-off-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

src/libcamera/camera.cpp
@@ -110,7 +110,8 @@ const std::string &Camera::name() const
  */
 
 Camera::Camera(PipelineHandler *pipe, const std::string &name)
-	: pipe_(pipe->shared_from_this()), name_(name), acquired_(false)
+	: pipe_(pipe->shared_from_this()), name_(name), acquired_(false),
+	  disconnected_(false)
 {
 }
 
@@ -133,7 +134,7 @@ void Camera::disconnect()
 {
 	LOG(Camera, Debug) << "Disconnecting camera " << name_;
 
-	/** \todo Block API calls when they will be implemented. */
+	disconnected_ = true;
 	disconnected.emit(this);
 }
 
@@ -184,6 +185,67 @@ void Camera::release()
 const std::vector<Stream *> &Camera::streams() const
 {
 	return streams_;
+}
+
+/**
+ * \brief Retrieve a group of stream configurations
+ * \param[in] streams A map of stream IDs and configurations to setup
+ *
+ * Retrieve the camera's configuration for a specified group of streams. The
+ * caller can specifies which of the camera's streams to retrieve configuration
+ * from by populating \a streams.
+ *
+ * The easiest way to populate the array of streams to fetch configuration from
+ * is to first retrieve the camera's full array of stream with streams() and
+ * then potentially trim it down to only contain the streams the caller
+ * are interested in.
+ *
+ * \return A map of successfully retrieved stream IDs and configurations or an
+ * empty list on error.
+ */
+std::map<Stream *, StreamConfiguration>
+Camera::streamConfiguration(std::vector<Stream *> &streams)
+{
+	if (disconnected_ || !streams.size())
+		std::map<unsigned int, StreamConfiguration> {};
+
+	return pipe_->streamConfiguration(this, streams);
+}
+
+/**
+ * \brief Configure the camera's streams prior to capture
+ * \param[in] config A map of stream IDs and configurations to setup
+ *
+ * Prior to starting capture, the camera must be configured to select a
+ * group of streams to be involved in the capture and their configuration.
+ * The caller specifies which streams are to be involved and their configuration
+ * by populating \a config.
+ *
+ * The easiest way to populate the array of config is to fetch an initial
+ * configuration from the camera with streamConfiguration() and then change the
+ * parameters to fit the caller's need and once all the streams parameters are
+ * configured hand that over to configureStreams() to actually setup the camera.
+ *
+ * Exclusive access to the camera shall be ensured by a call to acquire() prior
+ * to calling this function, otherwise an -EACCES error will be returned.
+ *
+ * \return 0 on success or a negative error code on error.
+ * \retval -ENODEV The camera is not connected to any hardware
+ * \retval -EACCES The user has not acquired exclusive access to the camera
+ * \retval -EINVAL The configuration is not valid
+ */
+int Camera::configureStreams(std::map<Stream *, StreamConfiguration> &config)
+{
+	if (disconnected_)
+		return -ENODEV;
+
+	if (!acquired_)
+		return -EACCES;
+
+	if (!config.size())
+		return -EINVAL;
+
+	return pipe_->configureStreams(this, config);
 }
 
 } /* namespace libcamera */

include/libcamera/camera.h
@@ -7,6 +7,7 @@
 #ifndef __LIBCAMERA_CAMERA_H__
 #define __LIBCAMERA_CAMERA_H__
 
+#include <map>
 #include <memory>
 #include <string>
 
@@ -16,6 +17,7 @@ namespace libcamera {
 
 class PipelineHandler;
 class Stream;
+class StreamConfiguration;
 
 class Camera final
 {
@@ -35,6 +37,9 @@ public:
 	void release();
 
 	const std::vector<Stream *> &streams() const;
+	std::map<Stream *, StreamConfiguration>
+	streamConfiguration(std::vector<Stream *> &streams);
+	int configureStreams(std::map<Stream *, StreamConfiguration> &config);
 
 private:
 	Camera(PipelineHandler *pipe, const std::string &name);
@@ -48,6 +53,7 @@ private:
 	std::vector<Stream *> streams_;
 
 	bool acquired_;
+	bool disconnected_;
 };
 
 } /* namespace libcamera */

commit 9aceed70a2879802c7fe00ea2c0455ccd5c14f07

libcamera: camera: fix bug in error check for streamConfiguration()
The return statement is missing altogether and an error is silently
ignored. Fix this and also update the empty map to use the correct type
for the map key.

Fixes: 65ea2422d24a3073 ("libcamera: camera: extend camera object to support configuration of streams")
Signed-off-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>
Reviewed-by: Jacopo Mondi <jacopo@jmondi.org>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

src/libcamera/camera.cpp
@@ -213,7 +213,7 @@ std::map<Stream *, StreamConfiguration>
 Camera::streamConfiguration(std::vector<Stream *> &streams)
 {
 	if (disconnected_ || !streams.size())
-		std::map<unsigned int, StreamConfiguration> {};
+		return std::map<Stream *, StreamConfiguration>{};
 
 	return pipe_->streamConfiguration(this, streams);
 }
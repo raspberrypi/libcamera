raspberrypi/libcamera

commit 9c907813321612e245ecdd25950959e8efbe9872

libcamera: camera: Log requested configuration in configureStreams()
The IPU3 pipeline handler logs the requested configuration in its
configureStreams() handler. This is useful for other pipeline handlers
as well, move it to the Camera class.

Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>
Reviewed-by: Jacopo Mondi <jacopo@jmondi.org>

src/libcamera/camera.cpp
@@ -5,6 +5,8 @@
  * camera.cpp - Camera device
  */
 
+#include <iomanip>
+
 #include <libcamera/camera.h>
 #include <libcamera/request.h>
 #include <libcamera/stream.h>
@@ -595,10 +597,22 @@ int Camera::configureStreams(const CameraConfiguration &config)
 		return -EINVAL;
 	}
 
+	std::ostringstream msg("configuring streams:");
+	unsigned int index = 0;
+
 	for (Stream *stream : config) {
 		if (streams_.find(stream) == streams_.end())
 			return -EINVAL;
+
+		const StreamConfiguration &cfg = config[stream];
+		msg << " (" << index << ") " << cfg.width << "x"
+		    << cfg.height << "-0x" << std::hex << std::setfill('0')
+		    << std::setw(8) << cfg.pixelFormat;
+
+		index++;
 	}
+
+	LOG(Camera, Info) << msg.str();
 
 	ret = pipe_->configureStreams(this, config);
 	if (ret)

src/libcamera/pipeline/ipu3/ipu3.cpp
@@ -244,12 +244,6 @@ int PipelineHandlerIPU3::configureStreams(Camera *camera,
 	ImgUDevice *imgu = data->imgu_;
 	int ret;
 
-	LOG(IPU3, Info)
-		<< "Requested image format " << cfg.width << "x"
-		<< cfg.height << "-0x" << std::hex << std::setfill('0')
-		<< std::setw(8) << cfg.pixelFormat << " on camera '"
-		<< camera->name() << "'";
-
 	/*
 	 * Verify that the requested size respects the IPU3 alignement
 	 * requirements (the image width shall be a multiple of 8 pixels and

commit 6647db6f42dbb080cb066b435591d72eb7bea770

libcamera: camera: Reset basefield to decimal
When logging the camera configuration, the same ostringstream instance
is used to assemble a message describing configuration of all the
configured streams.

After the first stream configuration has been assembled, the use of
std::hex modifies the ostringstream basefield, causing all successive
integers values inserted in the stream to be expressed as hexadecimals.

Fix that by resetting the stream's basefield to decimal, before
assembling a stream configuration description.

Before this patch:
INFO Camera camera.cpp:615  (0) 640x480-0x3231564e (1) 140xa0-0x3231564e
After this patch:
INFO Camera camera.cpp:616  (0) 640x480-0x3231564e (1) 320x160-0x3231564e

Fixes: 9c9078133216 ("libcamera: camera: Log requested configuration in configureStreams()")
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Signed-off-by: Jacopo Mondi <jacopo@jmondi.org>

src/libcamera/camera.cpp
@@ -597,7 +597,7 @@ int Camera::configureStreams(const CameraConfiguration &config)
 			return -EINVAL;
 
 		const StreamConfiguration &cfg = config[stream];
-		msg << " (" << index << ") " << cfg.toString();
+		msg << std::dec << " (" << index << ") " << cfg.toString();
 
 		index++;
 	}
0c2f97a0aba1fc144a33a4769e0b96373bb26318
Paul Elder <paul.elder@ideasonboard.com>
2019-06-05 16:40:28 -0400

    libcamera: ipa_module_info: remove cplusplus guards
    IPA modules must be implemented in C++, which means we no longer need
    __cplusplus guards, so remove them.
    
    Signed-off-by: Paul Elder <paul.elder@ideasonboard.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- include/libcamera/ipa/ipa_module_info.h
+++ include/libcamera/ipa/ipa_module_info.h
@@ -11,9 +11,7 @@
 
 #define IPA_MODULE_API_VERSION 1
 
-#ifdef __cplusplus
 namespace libcamera {
-#endif
 
 struct IPAModuleInfo {
 	int moduleAPIVersion;
@@ -22,16 +20,10 @@ struct IPAModuleInfo {
 	char name[256];
 } __attribute__((packed));
 
-#ifdef __cplusplus
 extern "C" {
-#endif
 extern const struct IPAModuleInfo ipaModuleInfo;
-#ifdef __cplusplus
 };
-#endif
 
-#ifdef __cplusplus
 }; /* namespace libcamera */
-#endif
 
 #endif /* __LIBCAMERA_IPA_MODULE_INFO_H__ */

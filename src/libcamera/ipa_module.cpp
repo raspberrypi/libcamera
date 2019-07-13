raspberrypi/libcamera

commit 099815b85377ac68147e789f491ef26c57da3956

libcamera: ipa_module: add isOpenSource
Add a method to IPAModule to check if the module is open source.
This uses the license field of the member IPAModuleInfo.

Signed-off-by: Paul Elder <paul.elder@ideasonboard.com>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

src/libcamera/ipa_module.cpp
@@ -7,6 +7,8 @@
 
 #include "ipa_module.h"
 
+#include <algorithm>
+#include <array>
 #include <dlfcn.h>
 #include <elf.h>
 #include <errno.h>
@@ -467,6 +469,28 @@ bool IPAModule::match(PipelineHandler *pipe,
 	return info_.pipelineVersion >= minVersion &&
 	       info_.pipelineVersion <= maxVersion &&
 	       !strcmp(info_.pipelineName, pipe->name());
+}
+
+/**
+ * \brief Verify if the IPA module is open source
+ *
+ * \sa IPAModuleInfo::license
+ */
+bool IPAModule::isOpenSource() const
+{
+	static std::array<const char *, sizeof(char *)> osLicenses = {
+		"GPL-2.0-only",
+		"GPL-2.0-or-later",
+		"GPL-3.0-only",
+		"GPL-3.0-or-later",
+		"LGPL-2.1-only",
+		"LGPL-2.1-or-later",
+		"LGPL-3.0-only",
+		"LGPL-3.0-or-later",
+	};
+
+	return std::find(osLicenses.begin(), osLicenses.end(), info_.license)
+	       != osLicenses.end();
 }
 
 } /* namespace libcamera */

src/libcamera/include/ipa_module.h
@@ -35,6 +35,8 @@ public:
 	bool match(PipelineHandler *pipe,
 		   uint32_t minVersion, uint32_t maxVersion) const;
 
+	bool isOpenSource() const;
+
 private:
 	struct IPAModuleInfo info_;
 

commit 8f1dbd6a92381e38ba4bf65bdc35d5475ae6dc0a

libcamera: ipa_module: Fix open source license verification
The second argument to std::array is the size of the array, not of the
elements it contains. Fix this by turning the std::array into a simple
array of const char pointers.

Fixes: 099815b85377ac68 ("libcamera: ipa_module: add isOpenSource")
Signed-off-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>
Reviewed-by: Paul Elder <paul.elder@ideasonboard.com>

src/libcamera/ipa_module.cpp
@@ -22,6 +22,7 @@
 
 #include "log.h"
 #include "pipeline_handler.h"
+#include "utils.h"
 
 /**
  * \file ipa_module.h
@@ -478,7 +479,7 @@ bool IPAModule::match(PipelineHandler *pipe,
  */
 bool IPAModule::isOpenSource() const
 {
-	static std::array<const char *, sizeof(char *)> osLicenses = {
+	static const char *osLicenses[] = {
 		"GPL-2.0-only",
 		"GPL-2.0-or-later",
 		"GPL-3.0-only",
@@ -489,8 +490,11 @@ bool IPAModule::isOpenSource() const
 		"LGPL-3.0-or-later",
 	};
 
-	return std::find(osLicenses.begin(), osLicenses.end(), info_.license)
-	       != osLicenses.end();
+	for (unsigned int i = 0; i < ARRAY_SIZE(osLicenses); i++)
+		if (!strcmp(osLicenses[i], info_.license))
+			return true;
+
+	return false;
 }
 
 } /* namespace libcamera */
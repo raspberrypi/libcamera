f671d84ceb491fdb07ad39d1fe20950fd6482e4f
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2019-09-28 02:45:02 +0300

    libcamera: Add ControlValidator implementation for Camera
    Add a new CameraControlValidator class that implements the
    ControlValidator interface for a Camera object.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>

---
+++ src/libcamera/camera_controls.cpp
@@ -0,0 +1,53 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2019, Google Inc.
+ *
+ * camera_controls.cpp - Camera controls
+ */
+
+#include "camera_controls.h"
+
+#include <libcamera/camera.h>
+#include <libcamera/controls.h>
+
+/**
+ * \file camera_controls.h
+ * \brief Controls for Camera instances
+ */
+
+namespace libcamera {
+
+/**
+ * \class CameraControlValidator
+ * \brief A control validator for Camera instances
+ *
+ * This ControlValidator specialisation validates that controls exist in the
+ * Camera associated with the validator.
+ */
+
+/**
+ * \brief Construst a CameraControlValidator for the \a camera
+ * \param[in] camera The camera
+ */
+CameraControlValidator::CameraControlValidator(Camera *camera)
+	: camera_(camera)
+{
+}
+
+const std::string &CameraControlValidator::name() const
+{
+	return camera_->name();
+}
+
+/**
+ * \brief Validate a control
+ * \param[in] id The control ID
+ * \return True if the control is valid, false otherwise
+ */
+bool CameraControlValidator::validate(const ControlId &id) const
+{
+	const ControlInfoMap &controls = camera_->controls();
+	return controls.find(&id) != controls.end();
+}
+
+} /* namespace libcamera */
---
+++ src/libcamera/include/camera_controls.h
@@ -0,0 +1,30 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2019, Google Inc.
+ *
+ * camera_controls.h - Camera controls
+ */
+#ifndef __LIBCAMERA_CAMERA_CONTROLS_H__
+#define __LIBCAMERA_CAMERA_CONTROLS_H__
+
+#include "control_validator.h"
+
+namespace libcamera {
+
+class Camera;
+
+class CameraControlValidator final : public ControlValidator
+{
+public:
+	CameraControlValidator(Camera *camera);
+
+	const std::string &name() const override;
+	bool validate(const ControlId &id) const override;
+
+private:
+	Camera *camera_;
+};
+
+} /* namespace libcamera */
+
+#endif /* __LIBCAMERA_CAMERA_CONTROLS_H__ */

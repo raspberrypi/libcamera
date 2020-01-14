raspberrypi/libcamera

commit 4b9bd6c3ad94c617ac00a71d2e1ef33648afc99f

libcamera: ipa_interface: Document the ownership of dmabufs passed to map_buffers()
The ownership of the dmabuf file handles passed to map_buffers() is not
clear. Explicitly document that they are borrowed from the caller and
only guaranteed to be valid for the duration of the map_buffers() call.

Signed-off-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

src/libcamera/ipa_interface.cpp
@@ -258,6 +258,11 @@
  * \param[in] buffers The buffers to map
  * \param[in] num_buffers The number of entries in the \a buffers array
  *
+ * The dmabuf file descriptors provided in \a buffers are borrowed from the
+ * caller and are only guaranteed to be valid during the map_buffers() call.
+ * Should the callee need to store a copy of the file descriptors, it shall
+ * duplicate them first with ::dup().
+ *
  * \sa libcamera::IPAInterface::mapBuffers()
  */
 

commit 744fabcbb938d60cdbc0dfadc625bbd6798c2485

libcamera: ipa_interface: Fix doxygen warning
Doxygen generates the following warning:

src/libcamera/ipa_interface.cpp:262: warning: explicit link request to 'dup()' could not be resolved

Fix it by disabling link generation by prefixing the function name with
a %.

Fixes: 4b9bd6c3ad94 ("libcamera: ipa_interface: Document the ownership of dmabufs passed to map_buffers()")
Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Reviewed-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>

src/libcamera/ipa_interface.cpp
@@ -261,7 +261,7 @@
  * The dmabuf file descriptors provided in \a buffers are borrowed from the
  * caller and are only guaranteed to be valid during the map_buffers() call.
  * Should the callee need to store a copy of the file descriptors, it shall
- * duplicate them first with ::dup().
+ * duplicate them first with ::%dup().
  *
  * \sa libcamera::IPAInterface::mapBuffers()
  */
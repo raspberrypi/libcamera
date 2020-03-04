c153be4fecbe01364010fe4c6b5ce835e081d1c4
Jacopo Mondi <jacopo@jmondi.org>
2020-03-04 17:16:32 +0100

    test: v4l2_videodevice: Test U8 array controls
    Test V4L2 array control using vivid control VIVID_CID_U8_4D_ARRAY.
    
    Signed-off-by: Jacopo Mondi <jacopo@jmondi.org>
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Jacopo Mondi <jacopo@jmondi.org>

--- test/v4l2_videodevice/controls.cpp
+++ test/v4l2_videodevice/controls.cpp
@@ -5,12 +5,21 @@
  * controls.cpp - V4L2 device controls handling test
  */
 
+#include <algorithm>
+#include <array>
 #include <iostream>
 #include <limits.h>
 
 #include "v4l2_videodevice.h"
 
 #include "v4l2_videodevice_test.h"
+
+/* These come from the vivid driver. */
+#define VIVID_CID_CUSTOM_BASE		(V4L2_CID_USER_BASE | 0xf000)
+#define VIVID_CID_U8_4D_ARRAY		(VIVID_CID_CUSTOM_BASE + 10)
+
+/* Helper for VIVID_CID_U8_4D_ARRAY control array size: not from kernel. */
+#define VIVID_CID_U8_ARRAY_SIZE		(2 * 3 * 4 * 5)
 
 using namespace std;
 using namespace libcamera;
@@ -36,7 +45,8 @@ protected:
 
 		if (infoMap.find(V4L2_CID_BRIGHTNESS) == infoMap.end() ||
 		    infoMap.find(V4L2_CID_CONTRAST) == infoMap.end() ||
-		    infoMap.find(V4L2_CID_SATURATION) == infoMap.end()) {
+		    infoMap.find(V4L2_CID_SATURATION) == infoMap.end() ||
+		    infoMap.find(VIVID_CID_U8_4D_ARRAY) == infoMap.end()) {
 			cerr << "Missing controls" << endl;
 			return TestFail;
 		}
@@ -44,12 +54,14 @@ protected:
 		const ControlInfo &brightness = infoMap.find(V4L2_CID_BRIGHTNESS)->second;
 		const ControlInfo &contrast = infoMap.find(V4L2_CID_CONTRAST)->second;
 		const ControlInfo &saturation = infoMap.find(V4L2_CID_SATURATION)->second;
+		const ControlInfo &u8 = infoMap.find(VIVID_CID_U8_4D_ARRAY)->second;
 
 		/* Test getting controls. */
 		ControlList ctrls(infoMap);
 		ctrls.set(V4L2_CID_BRIGHTNESS, -1);
 		ctrls.set(V4L2_CID_CONTRAST, -1);
 		ctrls.set(V4L2_CID_SATURATION, -1);
+		ctrls.set(VIVID_CID_U8_4D_ARRAY, 0);
 
 		int ret = capture_->getControls(&ctrls);
 		if (ret) {
@@ -64,10 +76,26 @@ protected:
 			return TestFail;
 		}
 
+		uint8_t u8Min = u8.min().get<uint8_t>();
+		uint8_t u8Max = u8.max().get<uint8_t>();
+
+		Span<const uint8_t> u8Span = ctrls.get(VIVID_CID_U8_4D_ARRAY).get<Span<const uint8_t>>();
+		bool valid = std::all_of(u8Span.begin(), u8Span.end(),
+					 [&](uint8_t v) { return v >= u8Min && v <= u8Max; });
+		if (!valid) {
+			cerr << "Incorrect value for retrieved array control"
+			     << endl;
+			return TestFail;
+		}
+
 		/* Test setting controls. */
 		ctrls.set(V4L2_CID_BRIGHTNESS, brightness.min());
 		ctrls.set(V4L2_CID_CONTRAST, contrast.max());
 		ctrls.set(V4L2_CID_SATURATION, saturation.min());
+
+		std::array<uint8_t, VIVID_CID_U8_ARRAY_SIZE> u8Values;
+		std::fill(u8Values.begin(), u8Values.end(), u8.min().get<uint8_t>());
+		ctrls.set(VIVID_CID_U8_4D_ARRAY, Span<const uint8_t>(u8Values));
 
 		ret = capture_->setControls(&ctrls);
 		if (ret) {

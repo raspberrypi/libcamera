raspberrypi/libcamera

commit a14b6baca9afe5ad15b49a3ff25b01a6f582689e

cam: Add option to list camera properties
Add the '-p'|'--list-properties' option to the cam application to list
the properties of a camera.

Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Signed-off-by: Jacopo Mondi <jacopo@jmondi.org>
Reviewed-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/cam/main.cpp
@@ -11,6 +11,7 @@
 #include <string.h>
 
 #include <libcamera/libcamera.h>
+#include <libcamera/property_ids.h>
 
 #include "capture.h"
 #include "event_loop.h"
@@ -36,6 +37,7 @@ public:
 private:
 	int parseOptions(int argc, char *argv[]);
 	int prepareConfig();
+	int listProperties();
 	int infoConfiguration();
 	int run();
 
@@ -180,6 +182,8 @@ int CamApp::parseOptions(int argc, char *argv[])
 	parser.addOption(OptInfo, OptionNone,
 			 "Display information about stream(s)", "info");
 	parser.addOption(OptList, OptionNone, "List all cameras", "list");
+	parser.addOption(OptProps, OptionNone, "List cameras properties",
+			 "list-properties");
 
 	options_ = parser.parse(argc, argv);
 	if (!options_.valid())
@@ -268,6 +272,24 @@ int CamApp::prepareConfig()
 	return 0;
 }
 
+int CamApp::listProperties()
+{
+	if (!camera_) {
+		std::cout << "Cannot list properties without a camera"
+			  << std::endl;
+		return -EINVAL;
+	}
+
+	for (const auto &prop : camera_->properties()) {
+		const ControlId *id = properties::properties.at(prop.first);
+		const ControlValue &value = prop.second;
+
+		std::cout << "Property: " << id->name() << " = " << value.toString();
+	}
+
+	return 0;
+}
+
 int CamApp::infoConfiguration()
 {
 	if (!config_) {
@@ -310,6 +332,12 @@ int CamApp::run()
 			std::cout << index << ": " << cam->name() << std::endl;
 			index++;
 		}
+	}
+
+	if (options_.isSet(OptProps)) {
+		ret = listProperties();
+		if (ret)
+			return ret;
 	}
 
 	if (options_.isSet(OptInfo)) {

src/cam/main.h
@@ -14,6 +14,7 @@ enum {
 	OptHelp = 'h',
 	OptInfo = 'I',
 	OptList = 'l',
+	OptProps = 'p',
 	OptStream = 's',
 };
 

commit cf66c4406bbd66b885090d2ddbe2b72b6fb0492d

cam: Print one property per line
When printing camera properties, print one property per line.

Fixes: a14b6baca9af ("cam: Add option to list camera properties")
Reviewed-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>
Signed-off-by: Jacopo Mondi <jacopo@jmondi.org>

src/cam/main.cpp
@@ -284,7 +284,8 @@ int CamApp::listProperties()
 		const ControlId *id = properties::properties.at(prop.first);
 		const ControlValue &value = prop.second;
 
-		std::cout << "Property: " << id->name() << " = " << value.toString();
+		std::cout << "Property: " << id->name() << " = "
+			  << value.toString() << std::endl;
 	}
 
 	return 0;
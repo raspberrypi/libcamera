43d81d43fe91defcadec9f2f4f01e5afe9852ec5
Naushir Patuck <naush@raspberrypi.com>
2020-03-26 10:35:33 +0000

    include: uapi: Add header definitions for BCM2835 Unicam and ISP blocks
    This commit adds the headers and definitions required for the
    bcm2835_isp and bcm2835_unicam kernel modules.
    
    The headers come from patches recently posted to the
    linux-media@vger.kernel.org mailing list to add the Unicam and ISP
    peripherals drivers to the kernel.
    
    Signed-off-by: Naushir Patuck <naush@raspberrypi.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- include/linux/videodev2.h
+++ include/linux/videodev2.h
@@ -751,6 +751,8 @@ struct v4l2_pix_format {
 #define V4L2_META_FMT_VSP1_HGT    v4l2_fourcc('V', 'S', 'P', 'T') /* R-Car VSP1 2-D Histogram */
 #define V4L2_META_FMT_UVC         v4l2_fourcc('U', 'V', 'C', 'H') /* UVC Payload Header metadata */
 #define V4L2_META_FMT_D4XX        v4l2_fourcc('D', '4', 'X', 'X') /* D4XX Payload Header metadata */
+#define V4L2_META_FMT_SENSOR_DATA v4l2_fourcc('S', 'E', 'N', 'S') /* Sensor Ancillary metadata */
+#define V4L2_META_FMT_BCM2835_ISP_STATS v4l2_fourcc('B', 'S', 'T', 'A') /* BCM2835 ISP image statistics output */
 
 /* Vendor specific - used for RK_ISP1 camera sub-system */
 #define V4L2_META_FMT_RK_ISP1_PARAMS	v4l2_fourcc('R', 'K', '1', 'P') /* Rockchip ISP1 params */

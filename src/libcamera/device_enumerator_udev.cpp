raspberrypi/libcamera

commit e75ef59e028e5b686d686789439ede443c9c4cde

libcamera: device_enumerator_udev: Update pending list in addUdevDevice
Media devices that have unmet dependencies are added to the pending list
in populateMediaDevice(). Move the code to the caller, addUdevDevice(),
as it logically belongs there.

Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/libcamera/device_enumerator_udev.cpp
@@ -81,7 +81,8 @@ int DeviceEnumeratorUdev::addUdevDevice(struct udev_device *dev)
 		if (!media)
 			return -ENODEV;
 
-		int ret = populateMediaDevice(media);
+		DependencyMap deps;
+		int ret = populateMediaDevice(media.get(), &deps);
 		if (ret < 0) {
 			LOG(DeviceEnumerator, Warning)
 				<< "Failed to populate media device "
@@ -90,10 +91,16 @@ int DeviceEnumeratorUdev::addUdevDevice(struct udev_device *dev)
 			return ret;
 		}
 
-		if (ret) {
+		if (!deps.empty()) {
 			LOG(DeviceEnumerator, Debug)
 				<< "Defer media device " << media->deviceNode()
 				<< " due to " << ret << " missing dependencies";
+
+			pending_.emplace_back(media, deps);
+			MediaDeviceDeps *mediaDeps = &pending_.back();
+			for (const auto &dep : mediaDeps->deps_)
+				devMap_[dep.first] = mediaDeps;
+
 			return 0;
 		}
 
@@ -185,10 +192,9 @@ done:
 	return 0;
 }
 
-int DeviceEnumeratorUdev::populateMediaDevice(const std::shared_ptr<MediaDevice> &media)
+int DeviceEnumeratorUdev::populateMediaDevice(MediaDevice *media, DependencyMap *deps)
 {
 	std::set<dev_t> children;
-	DependencyMap deps;
 
 	/* Associate entities to device node paths. */
 	for (MediaEntity *entity : media->entities()) {
@@ -203,7 +209,7 @@ int DeviceEnumeratorUdev::populateMediaDevice(const std::shared_ptr<MediaDevice>
 		 * dependencies.
 		 */
 		if (orphans_.find(devnum) == orphans_.end()) {
-			deps[devnum].push_back(entity);
+			(*deps)[devnum].push_back(entity);
 			continue;
 		}
 
@@ -231,17 +237,7 @@ int DeviceEnumeratorUdev::populateMediaDevice(const std::shared_ptr<MediaDevice>
 			++it;
 	}
 
-	/*
-	 * If the media device has unmet dependencies, add it to the pending
-	 * list and update the devnum map accordingly.
-	 */
-	if (!deps.empty()) {
-		pending_.emplace_back(media, deps);
-		for (const auto &dep : deps)
-			devMap_[dep.first] = &pending_.back();
-	}
-
-	return deps.size();
+	return 0;
 }
 
 /**

src/libcamera/include/device_enumerator_udev.h
@@ -63,7 +63,7 @@ private:
 	std::map<dev_t, MediaDeviceDeps *> devMap_;
 
 	int addUdevDevice(struct udev_device *dev);
-	int populateMediaDevice(const std::shared_ptr<MediaDevice> &media);
+	int populateMediaDevice(MediaDevice *media, DependencyMap *deps);
 	std::string lookupDeviceNode(dev_t devnum);
 
 	int addV4L2Device(dev_t devnum);

commit 058407bb9ba4e122f464b3515f30ae1205ff803d

libcamera: device_enumerator_udev: Log the right number of missing deps
Since commit e75ef59e028e ("libcamera: device_enumerator_udev: Update
pending list in addUdevDevice") the populateMediaDevice() function
returns 0 on success instead of the number of missing dependencies,
resulting in a wrong number being logged. Fix it.

Fixes: e75ef59e028e ("libcamera: device_enumerator_udev: Update pending list in addUdevDevice")
Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Umang Jain <email@uajain.com>

src/libcamera/device_enumerator_udev.cpp
@@ -94,7 +94,8 @@ int DeviceEnumeratorUdev::addUdevDevice(struct udev_device *dev)
 		if (!deps.empty()) {
 			LOG(DeviceEnumerator, Debug)
 				<< "Defer media device " << media->deviceNode()
-				<< " due to " << ret << " missing dependencies";
+				<< " due to " << deps.size()
+				<< " missing dependencies";
 
 			pending_.emplace_back(std::move(media), std::move(deps));
 			MediaDeviceDeps *mediaDeps = &pending_.back();
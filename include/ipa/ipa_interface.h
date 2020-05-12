raspberrypi/libcamera

commit 319d6ae8e3ad1a47a674e85015de9ea811e84564

libcamera: controls: Merge ControlInfoMap and V4L2ControlInfoMap
The ControlInfoMap and V4L2ControlInfoMap classes are very similar, with
the latter adding convenience accessors based on numerical IDs for the
former, as well as a cached idmap. Both features can be useful for
ControlInfoMap in the context of serialisation, and merging the two
classes will further simplify the IPA API.

Import all the features of V4L2ControlInfoMap into ControlInfoMap,
turning the latter into a real class. A few new constructors and
assignment operators are added for completeness.

Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>
Reviewed-by: Jacopo Mondi <jacopo@jmondi.org>

include/ipa/ipa_interface.h
@@ -43,7 +43,7 @@ public:
 	virtual int init() = 0;
 
 	virtual void configure(const std::map<unsigned int, IPAStream> &streamConfig,
-			       const std::map<unsigned int, V4L2ControlInfoMap> &entityControls) = 0;
+			       const std::map<unsigned int, ControlInfoMap> &entityControls) = 0;
 
 	virtual void mapBuffers(const std::vector<IPABuffer> &buffers) = 0;
 	virtual void unmapBuffers(const std::vector<unsigned int> &ids) = 0;

include/libcamera/controls.h
@@ -118,7 +118,50 @@ private:
 };
 
 using ControlIdMap = std::unordered_map<unsigned int, const ControlId *>;
-using ControlInfoMap = std::unordered_map<const ControlId *, ControlRange>;
+
+class ControlInfoMap : private std::unordered_map<const ControlId *, ControlRange>
+{
+public:
+	using Map = std::unordered_map<const ControlId *, ControlRange>;
+
+	ControlInfoMap() = default;
+	ControlInfoMap(const ControlInfoMap &other) = default;
+	ControlInfoMap(std::initializer_list<Map::value_type> init);
+
+	ControlInfoMap &operator=(const ControlInfoMap &other) = default;
+	ControlInfoMap &operator=(std::initializer_list<Map::value_type> init);
+	ControlInfoMap &operator=(Map &&info);
+
+	using Map::key_type;
+	using Map::mapped_type;
+	using Map::value_type;
+	using Map::size_type;
+	using Map::iterator;
+	using Map::const_iterator;
+
+	using Map::begin;
+	using Map::cbegin;
+	using Map::end;
+	using Map::cend;
+	using Map::at;
+	using Map::empty;
+	using Map::size;
+	using Map::count;
+	using Map::find;
+
+	mapped_type &at(unsigned int key);
+	const mapped_type &at(unsigned int key) const;
+	size_type count(unsigned int key) const;
+	iterator find(unsigned int key);
+	const_iterator find(unsigned int key) const;
+
+	const ControlIdMap &idmap() const { return idmap_; }
+
+private:
+	void generateIdmap();
+
+	ControlIdMap idmap_;
+};
 
 class ControlList
 {

src/ipa/ipa_vimc.cpp
@@ -31,7 +31,7 @@ public:
 
 	int init() override;
 	void configure(const std::map<unsigned int, IPAStream> &streamConfig,
-		       const std::map<unsigned int, V4L2ControlInfoMap> &entityControls) override {}
+		       const std::map<unsigned int, ControlInfoMap> &entityControls) override {}
 	void mapBuffers(const std::vector<IPABuffer> &buffers) override {}
 	void unmapBuffers(const std::vector<unsigned int> &ids) override {}
 	void processEvent(const IPAOperationData &event) override {}

src/ipa/rkisp1/rkisp1.cpp
@@ -33,7 +33,7 @@ public:
 	int init() override { return 0; }
 
 	void configure(const std::map<unsigned int, IPAStream> &streamConfig,
-		       const std::map<unsigned int, V4L2ControlInfoMap> &entityControls) override;
+		       const std::map<unsigned int, ControlInfoMap> &entityControls) override;
 	void mapBuffers(const std::vector<IPABuffer> &buffers) override;
 	void unmapBuffers(const std::vector<unsigned int> &ids) override;
 	void processEvent(const IPAOperationData &event) override;
@@ -49,7 +49,7 @@ private:
 
 	std::map<unsigned int, BufferMemory> bufferInfo_;
 
-	V4L2ControlInfoMap ctrls_;
+	ControlInfoMap ctrls_;
 
 	/* Camera sensor controls. */
 	bool autoExposure_;
@@ -62,7 +62,7 @@ private:
 };
 
 void IPARkISP1::configure(const std::map<unsigned int, IPAStream> &streamConfig,
-			  const std::map<unsigned int, V4L2ControlInfoMap> &entityControls)
+			  const std::map<unsigned int, ControlInfoMap> &entityControls)
 {
 	if (entityControls.empty())
 		return;

src/libcamera/camera_sensor.cpp
@@ -251,7 +251,7 @@ int CameraSensor::setFormat(V4L2SubdeviceFormat *format)
  * \brief Retrieve the supported V4L2 controls and their information
  * \return A map of the V4L2 controls supported by the sensor
  */
-const V4L2ControlInfoMap &CameraSensor::controls() const
+const ControlInfoMap &CameraSensor::controls() const
 {
 	return subdev_->controls();
 }

src/libcamera/controls.cpp
@@ -393,9 +393,146 @@ std::string ControlRange::toString() const
  */
 
 /**
- * \typedef ControlInfoMap
+ * \class ControlInfoMap
  * \brief A map of ControlId to ControlRange
+ *
+ * The ControlInfoMap class describes controls supported by an object as an
+ * unsorted map of ControlId pointers to ControlRange instances. Unlike the
+ * standard std::unsorted_map<> class, it is designed the be immutable once
+ * constructed, and thus only exposes the read accessors of the
+ * std::unsorted_map<> base class.
+ *
+ * In addition to the features of the standard unsorted map, this class also
+ * provides access to the mapped elements using numerical ID keys. It maintains
+ * an internal map of numerical ID to ControlId for this purpose, and exposes it
+ * through the idmap() method to help construction of ControlList instances.
  */
+
+/**
+ * \typedef ControlInfoMap::Map
+ * \brief The base std::unsorted_map<> container
+ */
+
+/**
+ * \fn ControlInfoMap::ControlInfoMap(const ControlInfoMap &other)
+ * \brief Copy constructor, construct a ControlInfoMap from a copy of \a other
+ * \param[in] other The other ControlInfoMap
+ */
+
+/**
+ * \brief Construct a ControlInfoMap from an initializer list
+ * \param[in] init The initializer list
+ */
+ControlInfoMap::ControlInfoMap(std::initializer_list<Map::value_type> init)
+	: Map(init)
+{
+	generateIdmap();
+}
+
+/**
+ * \fn ControlInfoMap &ControlInfoMap::operator=(const ControlInfoMap &other)
+ * \brief Copy assignment operator, replace the contents with a copy of \a other
+ * \param[in] other The other ControlInfoMap
+ * \return A reference to the ControlInfoMap
+ */
+
+/**
+ * \brief Replace the contents with those from the initializer list
+ * \param[in] init The initializer list
+ * \return A reference to the ControlInfoMap
+ */
+ControlInfoMap &ControlInfoMap::operator=(std::initializer_list<Map::value_type> init)
+{
+	Map::operator=(init);
+	generateIdmap();
+	return *this;
+}
+
+/**
+ * \brief Move assignment operator from a plain map
+ * \param[in] info The control info plain map
+ *
+ * Populate the map by replacing its contents with those of \a info using move
+ * semantics. Upon return the \a info map will be empty.
+ *
+ * \return A reference to the populated ControlInfoMap
+ */
+ControlInfoMap &ControlInfoMap::operator=(Map &&info)
+{
+	Map::operator=(std::move(info));
+	generateIdmap();
+	return *this;
+}
+
+/**
+ * \brief Access specified element by numerical ID
+ * \param[in] id The numerical ID
+ * \return A reference to the element whose ID is equal to \a id
+ */
+ControlInfoMap::mapped_type &ControlInfoMap::at(unsigned int id)
+{
+	return at(idmap_.at(id));
+}
+
+/**
+ * \brief Access specified element by numerical ID
+ * \param[in] id The numerical ID
+ * \return A const reference to the element whose ID is equal to \a id
+ */
+const ControlInfoMap::mapped_type &ControlInfoMap::at(unsigned int id) const
+{
+	return at(idmap_.at(id));
+}
+
+/**
+ * \brief Count the number of elements matching a numerical ID
+ * \param[in] id The numerical ID
+ * \return The number of elements matching the numerical \a id
+ */
+ControlInfoMap::size_type ControlInfoMap::count(unsigned int id) const
+{
+	return count(idmap_.at(id));
+}
+
+/**
+ * \brief Find the element matching a numerical ID
+ * \param[in] id The numerical ID
+ * \return An iterator pointing to the element matching the numerical \a id, or
+ * end() if no such element exists
+ */
+ControlInfoMap::iterator ControlInfoMap::find(unsigned int id)
+{
+	return find(idmap_.at(id));
+}
+
+/**
+ * \brief Find the element matching a numerical ID
+ * \param[in] id The numerical ID
+ * \return A const iterator pointing to the element matching the numerical
+ * \a id, or end() if no such element exists
+ */
+ControlInfoMap::const_iterator ControlInfoMap::find(unsigned int id) const
+{
+	return find(idmap_.at(id));
+}
+
+/**
+ * \fn const ControlIdMap &ControlInfoMap::idmap() const
+ * \brief Retrieve the ControlId map
+ *
+ * Constructing ControlList instances for V4L2 controls requires a ControlIdMap
+ * for the V4L2 device that the control list targets. This helper method
+ * returns a suitable idmap for that purpose.
+ *
+ * \return The ControlId map
+ */
+
+void ControlInfoMap::generateIdmap()
+{
+	idmap_.clear();
+	for (const auto &ctrl : *this)
+		idmap_[ctrl.first->id()] = ctrl.first;
+}
 
 /**
  * \class ControlList

src/libcamera/include/camera_sensor.h
@@ -16,9 +16,9 @@
 
 namespace libcamera {
 
+class ControlInfoMap;
 class ControlList;
 class MediaEntity;
-class V4L2ControlInfoMap;
 class V4L2Subdevice;
 
 struct V4L2SubdeviceFormat;
@@ -43,7 +43,7 @@ public:
 				      const Size &size) const;
 	int setFormat(V4L2SubdeviceFormat *format);
 
-	const V4L2ControlInfoMap &controls() const;
+	const ControlInfoMap &controls() const;
 	int getControls(ControlList *ctrls);
 	int setControls(ControlList *ctrls);
 

src/libcamera/include/v4l2_controls.h
@@ -31,44 +31,10 @@ public:
 	V4L2ControlRange(const struct v4l2_query_ext_ctrl &ctrl);
 };
 
-class V4L2ControlInfoMap : private ControlInfoMap
-{
-public:
-	V4L2ControlInfoMap &operator=(ControlInfoMap &&info);
-
-	using ControlInfoMap::key_type;
-	using ControlInfoMap::mapped_type;
-	using ControlInfoMap::value_type;
-	using ControlInfoMap::size_type;
-	using ControlInfoMap::iterator;
-	using ControlInfoMap::const_iterator;
-
-	using ControlInfoMap::begin;
-	using ControlInfoMap::cbegin;
-	using ControlInfoMap::end;
-	using ControlInfoMap::cend;
-	using ControlInfoMap::at;
-	using ControlInfoMap::empty;
-	using ControlInfoMap::size;
-	using ControlInfoMap::count;
-	using ControlInfoMap::find;
-
-	mapped_type &at(unsigned int key);
-	const mapped_type &at(unsigned int key) const;
-	size_type count(unsigned int key) const;
-	iterator find(unsigned int key);
-	const_iterator find(unsigned int key) const;
-
-	const ControlIdMap &idmap() const { return idmap_; }
-
-private:
-	ControlIdMap idmap_;
-};
-
 class V4L2ControlList : public ControlList
 {
 public:
-	V4L2ControlList(const V4L2ControlInfoMap &info)
+	V4L2ControlList(const ControlInfoMap &info)
 		: ControlList(info.idmap())
 	{
 	}

commit 49295e549e471429afdd2cbf3bafa77b4284d830

ipa: Drop v4l2_controls.h from ipa_interface.h
The v4l2_controls.h header is internal, and isn't needed by
ipa_interface.h. It originates from the days when V4L2 controls were
handled through a different API. Drop it.

Fixes: 319d6ae8e3ad ("libcamera: controls: Merge ControlInfoMap and V4L2ControlInfoMap")
Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

include/ipa/ipa_interface.h
@@ -123,8 +123,6 @@ struct ipa_context *ipaCreate();
 #include <libcamera/geometry.h>
 #include <libcamera/signal.h>
 
-#include "v4l2_controls.h"
-
 namespace libcamera {
 
 struct IPASettings {
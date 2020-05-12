924778eb073c47a0defc7319e98029c712129ede
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2020-05-12 19:24:23 +0300

    ipa: Only sign IPA modules that are being installed
    The ipa-sign-install.sh script, run when installing libcamera, signs all
    IPA modules present in the module directory. This would result in
    third-party modules being signed if any are present in the directory.
    Fix it by explicitly passing the list of IPA modules to the
    ipa-sign-install.sh script.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Tested-by: Tomasz Figa <tfiga@chromium.org>
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>

--- src/ipa/ipa-sign-install.sh
+++ src/ipa/ipa-sign-install.sh
@@ -6,13 +6,17 @@
 #
 # ipa-sign-install.sh - Regenerate IPA module signatures when installing
 
-libdir=$1
-key=$2
+key=$1
+shift
+modules=$*
 
 ipa_sign=$(dirname "$0")/ipa-sign.sh
 
 echo "Regenerating IPA modules signatures"
 
-for module in "${MESON_INSTALL_DESTDIR_PREFIX}/${libdir}"/*.so ; do
-	"${ipa_sign}" "${key}" "${module}" "${module}.sign"
+for module in ${modules} ; do
+	module="${MESON_INSTALL_DESTDIR_PREFIX}/${module}"
+	if [ -f "${module}" ] ; then
+		"${ipa_sign}" "${key}" "${module}" "${module}.sign"
+	fi
 done

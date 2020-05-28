raspberrypi/libcamera

commit f890a57b7a06c0fab3bea593e4c0040bc5fcfd42

qcam: viewfinder: Add support for more native formats
Qt supports more 24-bit and 32-bit RGB formats for native painting. If
the frame buffer pixel format matches any of them, disable the converter
and create a QImage in the appropriate format.

Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/qcam/viewfinder.cpp
@@ -7,15 +7,29 @@
 
 #include "viewfinder.h"
 
+#include <stdint.h>
 #include <utility>
 
 #include <QImage>
 #include <QImageWriter>
+#include <QMap>
 #include <QMutexLocker>
 #include <QPainter>
 #include <QtDebug>
 
 #include "format_converter.h"
+
+static const QMap<uint32_t, QImage::Format> nativeFormats
+{
+#if QT_VERSION >= QT_VERSION_CHECK(5, 2, 0)
+	{ DRM_FORMAT_ABGR8888, QImage::Format_RGBA8888 },
+#endif
+	{ DRM_FORMAT_ARGB8888, QImage::Format_RGB32 },
+#if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0)
+	{ DRM_FORMAT_BGR888, QImage::Format_BGR888 },
+#endif
+	{ DRM_FORMAT_RGB888, QImage::Format_RGB888 },
+};
 
 ViewFinder::ViewFinder(QWidget *parent)
 	: QWidget(parent), buffer_(nullptr)
@@ -36,7 +50,7 @@ int ViewFinder::setFormat(const libcamera::PixelFormat &format,
 	 * If format conversion is needed, configure the converter and allocate
 	 * the destination image.
 	 */
-	if (format != DRM_FORMAT_ARGB8888) {
+	if (!nativeFormats.contains(format)) {
 		int ret = converter_.configure(format, size);
 		if (ret < 0)
 			return ret;
@@ -64,7 +78,7 @@ void ViewFinder::render(libcamera::FrameBuffer *buffer, MappedBuffer *map)
 	{
 		QMutexLocker locker(&mutex_);
 
-		if (format_ == DRM_FORMAT_ARGB8888) {
+		if (nativeFormats.contains(format_)) {
 			/*
 			 * If the frame format is identical to the display
 			 * format, create a QImage that references the frame
@@ -76,7 +90,8 @@ void ViewFinder::render(libcamera::FrameBuffer *buffer, MappedBuffer *map)
 			 * computing it naively
 			 */
 			image_ = QImage(memory, size_.width(), size_.height(),
-					size / size_.height(), QImage::Format_RGB32);
+					size / size_.height(),
+					nativeFormats[format_]);
 			std::swap(buffer, buffer_);
 		} else {
 			/*

commit 16c5f44c690a1d7d31b2e150d6e916cc6a999119

qcam: viewfinder: Use correct DRM/QImage mappings
When the native pixel formats supported by QT were introduced, the
RGB/BGR formats were inverted.

Swap the BGR888 and RGB888 mappings accordingly.

Fixes: f890a57b7a06 ("qcam: viewfinder: Add support for more native formats")
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

src/qcam/viewfinder.cpp
@@ -26,9 +26,9 @@ static const QMap<libcamera::PixelFormat, QImage::Format> nativeFormats
 #endif
 	{ libcamera::PixelFormat{ DRM_FORMAT_ARGB8888 }, QImage::Format_RGB32 },
 #if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0)
-	{ libcamera::PixelFormat{ DRM_FORMAT_BGR888 }, QImage::Format_BGR888 },
+	{ libcamera::PixelFormat{ DRM_FORMAT_RGB888 }, QImage::Format_BGR888 },
 #endif
-	{ libcamera::PixelFormat{ DRM_FORMAT_RGB888 }, QImage::Format_RGB888 },
+	{ libcamera::PixelFormat{ DRM_FORMAT_BGR888 }, QImage::Format_RGB888 },
 };
 
 ViewFinder::ViewFinder(QWidget *parent)
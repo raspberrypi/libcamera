652137ce89cf36c5d8a27a771f63f8180b38923b
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2020-09-04 11:17:04 +0300

    HACK: libcamera: Camera public destructor
    pybind11 needs a public destructor for Camera to be able to manage the
    shared_ptr. It's not clear why this is needed, and can it be fixed in
    pybind11.
    
    Looks like there's a suggested fix, which has not been merged:
    
    https://github.com/pybind/pybind11/pull/2067
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>

--- include/libcamera/camera.h
+++ include/libcamera/camera.h
@@ -114,12 +114,12 @@ public:
 	int start(const ControlList *controls = nullptr);
 	int stop();
 
+	~Camera();
 private:
 	LIBCAMERA_DISABLE_COPY(Camera)
 
 	Camera(std::unique_ptr<Private> d, const std::string &id,
 	       const std::set<Stream *> &streams);
-	~Camera();
 
 	friend class PipelineHandler;
 	void disconnect();

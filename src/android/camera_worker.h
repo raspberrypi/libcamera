8806863b0bc4498d098e16dda657ab6f8293d208
Jacopo Mondi <jacopo@jmondi.org>
2020-10-06 17:53:51 +0200

    android: camera_worker: Introduce CameraWorker
    The Android camera framework provides for each buffer part of a capture
    request an acquisition fence the camera HAL is supposed to wait on
    before using the buffer. As the libcamera HAL runs in the camera service
    thread, it is not possible to perform a synchronous wait there.
    
    Introduce a CameraWorker class that runs an internal thread to wait
    on a set of fences before queueing a capture request to the
    libcamera::Camera.
    
    Fences completion is handled through a simple poll, similar in
    implementation to the sync_wait() function provided by libdrm.
    
    Reviewed-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Jacopo Mondi <jacopo@jmondi.org>

---
+++ src/android/camera_worker.h
@@ -0,0 +1,63 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2020, Google Inc.
+ *
+ * camera_worker.h - Process capture requests on behalf of the Camera HAL
+ */
+#ifndef __ANDROID_CAMERA_WORKER_H__
+#define __ANDROID_CAMERA_WORKER_H__
+
+#include <memory>
+
+#include <libcamera/buffer.h>
+#include <libcamera/camera.h>
+#include <libcamera/object.h>
+#include <libcamera/request.h>
+#include <libcamera/stream.h>
+
+#include "libcamera/internal/thread.h"
+
+class CameraDevice;
+
+class CaptureRequest
+{
+public:
+	CaptureRequest(libcamera::Camera *camera, uint64_t cookie);
+
+	const std::vector<int> &fences() const { return acquireFences_; }
+
+	void addBuffer(libcamera::Stream *stream,
+		       libcamera::FrameBuffer *buffer, int fence);
+	void queue();
+
+private:
+	libcamera::Camera *camera_;
+	std::vector<int> acquireFences_;
+	std::unique_ptr<libcamera::Request> request_;
+};
+
+class CameraWorker
+{
+public:
+	CameraWorker();
+
+	void start();
+	void stop();
+
+	void queueRequest(CaptureRequest *request);
+
+private:
+	class Worker : public libcamera::Object
+	{
+	public:
+		void processRequest(CaptureRequest *request);
+
+	private:
+		int waitFence(int fence);
+	};
+
+	Worker worker_;
+	libcamera::Thread thread_;
+};
+
+#endif /* __ANDROID_CAMERA_WORKER_H__ */

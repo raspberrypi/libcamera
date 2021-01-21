raspberrypi/libcamera

commit 4f5d17f3a4f51137ae5a547056f32a8a58f81d79

utils: checkstyle.py: Make title and files properties of commit class
Make the API of the Commit class more explicit by exposing the title and
files as properties instead of through a get_info() method.

Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>
Reviewed-by: Paul Elder <paul.elder@ideasonboard.com>

utils/checkstyle.py
@@ -197,15 +197,24 @@ def parse_diff(diff):
 class Commit:
     def __init__(self, commit):
         self.commit = commit
+        self.__parse()
 
-    def get_info(self):
+    def __parse(self):
         # Get the commit title and list of files.
         ret = subprocess.run(['git', 'show', '--pretty=oneline', '--name-only',
                               self.commit],
                              stdout=subprocess.PIPE).stdout.decode('utf-8')
         files = ret.splitlines()
-        # Returning title and files list as a tuple
-        return files[0], files[1:]
+        self.__files = files[1:]
+        self.__title = files[0]
+
+    @property
+    def files(self):
+        return self.__files
+
+    @property
+    def title(self):
+        return self.__title
 
     def get_diff(self, top_level, filename):
         return subprocess.run(['git', 'diff', '%s~..%s' % (self.commit, self.commit),
@@ -221,10 +230,11 @@ class StagedChanges(Commit):
     def __init__(self):
         Commit.__init__(self, '')
 
-    def get_info(self):
+    def __parse(self):
         ret = subprocess.run(['git', 'diff', '--staged', '--name-only'],
                              stdout=subprocess.PIPE).stdout.decode('utf-8')
-        return "Staged changes", ret.splitlines()
+        self.__title = "Staged changes"
+        self.__files = ret.splitlines()
 
     def get_diff(self, top_level, filename):
         return subprocess.run(['git', 'diff', '--staged', '--',
@@ -236,15 +246,15 @@ class Amendment(StagedChanges):
     def __init__(self):
         StagedChanges.__init__(self)
 
-    def get_info(self):
+    def __parse(self):
         # Create a title using HEAD commit
         ret = subprocess.run(['git', 'show', '--pretty=oneline', '--no-patch'],
                              stdout=subprocess.PIPE).stdout.decode('utf-8')
-        title = 'Amendment of ' + ret.strip()
+        self.__title = 'Amendment of ' + ret.strip()
         # Extract the list of modified files
         ret = subprocess.run(['git', 'diff', '--staged', '--name-only', 'HEAD~'],
                              stdout=subprocess.PIPE).stdout.decode('utf-8')
-        return title, ret.splitlines()
+        self.__files = ret.splitlines()
 
     def get_diff(self, top_level, filename):
         return subprocess.run(['git', 'diff', '--staged', 'HEAD~', '--',
@@ -686,18 +696,16 @@ def check_file(top_level, commit, filename):
 
 
 def check_style(top_level, commit):
-    title, files = commit.get_info()
-
-    separator = '-' * len(title)
+    separator = '-' * len(commit.title)
     print(separator)
-    print(title)
+    print(commit.title)
     print(separator)
 
     # Filter out files we have no checker for.
     patterns = set()
     patterns.update(StyleChecker.all_patterns())
     patterns.update(Formatter.all_patterns())
-    files = [f for f in files if len([p for p in patterns if fnmatch.fnmatch(os.path.basename(f), p)])]
+    files = [f for f in commit.files if len([p for p in patterns if fnmatch.fnmatch(os.path.basename(f), p)])]
     if len(files) == 0:
         print("Commit doesn't touch source files, skipping")
         return 0

commit afbf0ec626db43b0117d28ef4f9b825a7d19929a

utils: checkstyle.py: Fix "protected" members in Commit class
The Commit class and subclasses were reworked in commit 4f5d17f3a4f5
("utils: checkstyle.py: Make title and files properties of commit
class") with the introduction of members of the base class that were
meant to be protected (not used externally, but accessible by
subclasses). They have been named with a '__' prefix for this purpose,
which was a bad choice as Python effectively replaces a leading '__'
with a literal '__classname__' prefix to make them private
(https://docs.python.org/3/tutorial/classes.html#private-variables). The
members accessed in the derived classes are thus different from the ones
in the base class.

Fix this by replacing the double underscore prefix with a single
underscore, which is a "weak internal use indicator" (as specified in
https://www.python.org/dev/peps/pep-0008/), closer to the protected
access specifier of C++.

Reported-by: Umang Jain <email@uajain.com>
Reported-by: Naushir Patuck <naush@raspberrypi.com>
Fixes: 4f5d17f3a4f5 ("utils: checkstyle.py: Make title and files properties of commit class")
Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>
Tested-by: Naushir Patuck <naush@raspberrypi.com>
Reviewed-by: Naushir Patuck <naush@raspberrypi.com>

utils/checkstyle.py
@@ -203,23 +203,23 @@ class CommitFile:
 class Commit:
     def __init__(self, commit):
         self.commit = commit
-        self.__parse()
+        self._parse()
 
-    def __parse(self):
+    def _parse(self):
         # Get the commit title and list of files.
         ret = subprocess.run(['git', 'show', '--pretty=oneline', '--name-status',
                               self.commit],
                              stdout=subprocess.PIPE).stdout.decode('utf-8')
         files = ret.splitlines()
-        self.__files = [CommitFile(f) for f in files[1:]]
-        self.__title = files[0]
+        self._files = [CommitFile(f) for f in files[1:]]
+        self._title = files[0]
 
     def files(self, filter='AM'):
-        return [f.filename for f in self.__files if f.status in filter]
+        return [f.filename for f in self._files if f.status in filter]
 
     @property
     def title(self):
-        return self.__title
+        return self._title
 
     def get_diff(self, top_level, filename):
         diff = subprocess.run(['git', 'diff', '%s~..%s' % (self.commit, self.commit),
@@ -236,11 +236,11 @@ class StagedChanges(Commit):
     def __init__(self):
         Commit.__init__(self, '')
 
-    def __parse(self):
+    def _parse(self):
         ret = subprocess.run(['git', 'diff', '--staged', '--name-status'],
                              stdout=subprocess.PIPE).stdout.decode('utf-8')
-        self.__title = "Staged changes"
-        self.__files = [CommitFile(f) for f in ret.splitlines()]
+        self._title = "Staged changes"
+        self._files = [CommitFile(f) for f in ret.splitlines()]
 
     def get_diff(self, top_level, filename):
         diff = subprocess.run(['git', 'diff', '--staged', '--',
@@ -253,15 +253,15 @@ class Amendment(StagedChanges):
     def __init__(self):
         StagedChanges.__init__(self)
 
-    def __parse(self):
+    def _parse(self):
         # Create a title using HEAD commit
         ret = subprocess.run(['git', 'show', '--pretty=oneline', '--no-patch'],
                              stdout=subprocess.PIPE).stdout.decode('utf-8')
-        self.__title = 'Amendment of ' + ret.strip()
+        self._title = 'Amendment of ' + ret.strip()
         # Extract the list of modified files
         ret = subprocess.run(['git', 'diff', '--staged', '--name-status', 'HEAD~'],
                              stdout=subprocess.PIPE).stdout.decode('utf-8')
-        self.__files = [CommitFile(f) for f in ret.splitlines()]
+        self._files = [CommitFile(f) for f in ret.splitlines()]
 
     def get_diff(self, top_level, filename):
         diff = subprocess.run(['git', 'diff', '--staged', 'HEAD~', '--',
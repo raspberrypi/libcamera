raspberrypi/libcamera

commit 8fffab46b80fd2712a48b9a29bfe711e8262bb90

utils: checkstyle.py: Add header add checker
Add a commit checker that ensures that all header files added to the
libcamera includes (public or internal) are accompanied by a
corresponding update of the meson.build file in the same directory.

Here's the output of the new checker when run against a commit that
forgot to update meson.build.

    $ ./utils/checkstyle.py b3383da79f1d
    ---------------------------------------------------------------------------------
    b3383da79f1d513b0d76db220a7104e1c1035e30 libcamera: buffer: Create a MappedBuffer
    ---------------------------------------------------------------------------------
    Header include/libcamera/internal/buffer.h added without corresponding update to include/libcamera/internal/meson.build
    ---
    1 potential issue detected, please review

In theory we could extend the checker to cover .cpp files too, but the
issue will be quite noticeable as meson won't build the file if
meson.build isn't updated. Header files are more tricky as problems
would only occur at when installing the headers (for public headers), or
would result in race conditions in the build. Both of those issues are
harder to catch.

Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>

utils/checkstyle.py
@@ -320,6 +320,50 @@ class CommitIssue(object):
         self.msg = msg
 
 
+class HeaderAddChecker(CommitChecker):
+    @classmethod
+    def check(cls, commit, top_level):
+        issues = []
+
+        meson_files = [f for f in commit.files('M')
+                       if os.path.basename(f) == 'meson.build']
+
+        for filename in commit.files('A'):
+            if not filename.startswith('include/libcamera/') or \
+               not filename.endswith('.h'):
+                continue
+
+            meson = os.path.dirname(filename) + '/meson.build'
+            header = os.path.basename(filename)
+
+            issue = CommitIssue('Header %s added without corresponding update to %s' %
+                                (filename, meson))
+
+            if meson not in meson_files:
+                issues.append(issue)
+                continue
+
+            diff = commit.get_diff(top_level, meson)
+            found = False
+
+            for hunk in diff:
+                for line in hunk.lines:
+                    if line[0] != '+':
+                        continue
+
+                    if line.find("'%s'" % header) != -1:
+                        found = True
+                        break
+
+                if found:
+                    break
+
+            if not found:
+                issues.append(issue)
+
+        return issues
+
+
 # ------------------------------------------------------------------------------
 # Style Checkers
 #

commit 0e2ca7256bbd6b042d10061c3bce50b669eb9fb7

utils: checkstyle.py: Handle renamed files in header add checker
The header add checker only handles added header, which makes it miss
issues when a header is renamed. Fix it.

Fixes: 8fffab46b80f ("utils: checkstyle.py: Add header add checker")
Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

utils/checkstyle.py
@@ -314,7 +314,7 @@ class HeaderAddChecker(CommitChecker):
         meson_files = [f for f in commit.files('M')
                        if os.path.basename(f) == 'meson.build']
 
-        for filename in commit.files('A'):
+        for filename in commit.files('AR'):
             if not filename.startswith('include/libcamera/') or \
                not filename.endswith('.h'):
                 continue
8f3a54ae6a53076ebba44b13dbe237e348bd6d27
David Plowman <david.plowman@raspberrypi.com>
2021-03-08 22:39:38 +0000

    ipa: raspberrypi: Add support for imx290/imx327 sensors
    imx290 and imx327 share the same kernel driver (imx290.c) and are
    therefore both recognised here as "imx290". We add the necessary
    CamHelper for these sensors, as well as a camera tuning file.
    
    The tuning was done with an Innomaker STARVIS IMX327LQR module. These
    have no IR cut filter so there is no proper colour tuning. However,
    you should obtain reasonable results for most modules using this
    sensor. Specific tunings for further modules can always be added
    subsequently.
    
    To use this sensor on the Raspberry Pi platform, please add
    
    dtoverlay=imx290,clock-frequency=74250000
    
    into your /boot/config.txt file.
    
    Signed-off-by: David Plowman <david.plowman@raspberrypi.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

---
+++ src/ipa/raspberrypi/cam_helper_imx290.cpp
@@ -0,0 +1,67 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (C) 2021, Raspberry Pi (Trading) Limited
+ *
+ * cam_helper_imx290.cpp - camera helper for imx290 sensor
+ */
+
+#include <math.h>
+
+#include "cam_helper.hpp"
+
+using namespace RPiController;
+
+class CamHelperImx290 : public CamHelper
+{
+public:
+	CamHelperImx290();
+	uint32_t GainCode(double gain) const override;
+	double Gain(uint32_t gain_code) const override;
+	void GetDelays(int &exposure_delay, int &gain_delay,
+		       int &vblank_delay) const override;
+	unsigned int HideFramesModeSwitch() const override;
+
+private:
+	/*
+	 * Smallest difference between the frame length and integration time,
+	 * in units of lines.
+	 */
+	static constexpr int frameIntegrationDiff = 2;
+};
+
+CamHelperImx290::CamHelperImx290()
+	: CamHelper(nullptr, frameIntegrationDiff)
+{
+}
+
+uint32_t CamHelperImx290::GainCode(double gain) const
+{
+	int code = 66.6667 * log10(gain);
+	return std::max(0, std::min(code, 0xf0));
+}
+
+double CamHelperImx290::Gain(uint32_t gain_code) const
+{
+	return pow(10, 0.015 * gain_code);
+}
+
+void CamHelperImx290::GetDelays(int &exposure_delay, int &gain_delay,
+				int &vblank_delay) const
+{
+	exposure_delay = 2;
+	gain_delay = 2;
+	vblank_delay = 2;
+}
+
+unsigned int CamHelperImx290::HideFramesModeSwitch() const
+{
+	/* After a mode switch, we seem to get 1 bad frame. */
+	return 1;
+}
+
+static CamHelper *Create()
+{
+	return new CamHelperImx290();
+}
+
+static RegisterCamHelper reg("imx290", &Create);

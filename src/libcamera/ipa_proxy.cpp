raspberrypi/libcamera

commit 70238ceca5b2f0be1330ecb9664ae0f59f151152

utils: ipc: proxy: Track IPA with a state machine
Asynchronous tasks can only be submitted while the IPA is running.

Further more, the shutdown sequence can not be tracked with a simple
running flag. We can also be in the state 'Stopping' where we have not
yet completed all events, but we must not commence anything new.

Refactor the running_ boolean into a stateful enum to track this.

Reviewed-by: Paul Elder <paul.elder@ideasonboard.com>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/libcamera/ipa_proxy.cpp
@@ -37,7 +37,7 @@ LOG_DEFINE_CATEGORY(IPAProxy)
  * \param[in] ipam The IPA module
  */
 IPAProxy::IPAProxy(IPAModule *ipam)
-	: valid_(false), ipam_(ipam)
+	: valid_(false), state_(ProxyStopped), ipam_(ipam)
 {
 }
 

include/libcamera/internal/ipa_proxy.h
@@ -17,6 +17,12 @@ namespace libcamera {
 
 class IPAModule;
 
+enum ProxyState {
+	ProxyStopped,
+	ProxyStopping,
+	ProxyRunning,
+};
+
 class IPAProxy : public IPAInterface
 {
 public:
@@ -31,6 +37,7 @@ protected:
 	std::string resolvePath(const std::string &file) const;
 
 	bool valid_;
+	ProxyState state_;
 
 private:
 	IPAModule *ipam_;

utils/ipc/generators/libcamera_templates/module_ipa_proxy.cpp.tmpl
@@ -45,8 +45,7 @@ namespace {{ns}} {
 {%- endif %}
 
 {{proxy_name}}::{{proxy_name}}(IPAModule *ipam, bool isolate)
-	: IPAProxy(ipam), running_(false),
-	  isolate_(isolate), seq_(0)
+	: IPAProxy(ipam), isolate_(isolate), seq_(0)
 {
 	LOG(IPAProxy, Debug)
 		<< "initializing {{module_name}} proxy: loading IPA from "
@@ -155,7 +154,7 @@ void {{proxy_name}}::recvMessage(const IPCMessage &data)
 
 	return {{ "_ret" if method|method_return_value != "void" }};
 {%- elif method.mojom_name == "start" %}
-	running_ = true;
+	state_ = ProxyRunning;
 	thread_.start();
 
 	{{ "return " if method|method_return_value != "void" -}}
@@ -173,7 +172,7 @@ void {{proxy_name}}::recvMessage(const IPCMessage &data)
 	{%- endfor -%}
 );
 {% elif method|is_async %}
-	ASSERT(running_);
+	ASSERT(state_ == ProxyRunning);
 	proxy_.invokeMethod(&ThreadProxy::{{method.mojom_name}}, ConnectionTypeQueued,
 	{%- for param in method|method_param_names -%}
 		{{param}}{{- ", " if not loop.last}}
@@ -226,7 +225,7 @@ void {{proxy_name}}::recvMessage(const IPCMessage &data)
 {% for method in interface_event.methods %}
 {{proxy_funcs.func_sig(proxy_name, method, "Thread")}}
 {
-	ASSERT(running_);
+	ASSERT(state_ != ProxyStopped);
 	{{method.mojom_name}}.emit({{method.parameters|params_comma_sep}});
 }
 

utils/ipc/generators/libcamera_templates/module_ipa_proxy.h.tmpl
@@ -104,7 +104,6 @@ private:
 		{{interface_name}} *ipa_;
 	};
 
-	bool running_;
 	Thread thread_;
 	ThreadProxy proxy_;
 	std::unique_ptr<{{interface_name}}> ipa_;

utils/ipc/generators/libcamera_templates/proxy_functions.tmpl
@@ -23,8 +23,11 @@
  # \brief Generate function body for IPA stop() function for thread
  #}
 {%- macro stop_thread_body() -%}
-	if (!running_)
+	ASSERT(state_ != ProxyStopping);
+	if (state_ != ProxyRunning)
 		return;
+
+	state_ = ProxyStopping;
 
 	proxy_.invokeMethod(&ThreadProxy::stop, ConnectionTypeBlocking);
 
@@ -33,7 +36,7 @@
 
 	Thread::current()->dispatchMessages(Message::Type::InvokeMessage);
 
-	running_ = false;
+	state_ = ProxyStopped;
 {%- endmacro -%}
 
 

commit 1c9f2d7889b1e158bfac320cc270f0f9314b779e

libcamera: ipa_proxy: Document ProxyState
The documentation for the ProxyState and tracking variable was not added
when the IPA was extended with a state machine.

Add it.

Fixes: 70238ceca5b2 ("utils: ipc: proxy: Track IPA with a state machine")
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/libcamera/ipa_proxy.cpp
@@ -33,6 +33,19 @@ LOG_DEFINE_CATEGORY(IPAProxy)
  */
 
 /**
+ * \enum IPAProxy::ProxyState
+ * \brief Identifies the available operational states of the proxy
+ *
+ * \var IPAProxy::ProxyStopped
+ * \brief The proxy is not active and only synchronous operations are permitted
+ * \var IPAProxy::ProxyStopping
+ * \brief No new tasks can be submitted to the proxy, however existing events
+ * can be completed
+ * \var IPAProxy::ProxyRunning
+ * \brief The Proxy is active and asynchronous tasks may be queued
+ */
+
+/**
  * \brief Construct an IPAProxy instance
  * \param[in] ipam The IPA module
  */
@@ -211,6 +224,17 @@ std::string IPAProxy::resolvePath(const std::string &file) const
  *
  * Implementations of the IPAProxy class should set this flag upon successful
  * construction.
+ */
+
+/**
+ * \var IPAProxy::state_
+ * \brief Current state of the IPAProxy
+ *
+ * The IPAProxy can be Running, Stopped, or Stopping.
+ *
+ * This state provides a means to ensure that asynchronous methods are only
+ * called while the proxy is running, and prevent new tasks being submitted
+ * while still enabling events to complete when the IPAProxy is stopping.
  */
 
 } /* namespace libcamera */
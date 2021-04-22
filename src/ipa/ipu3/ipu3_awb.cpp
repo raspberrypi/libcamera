7e0da7c34900cb219ee447df547162211d8683e3
Jean-Michel Hautbois <jeanmichel.hautbois@ideasonboard.com>
2021-04-22 17:46:26 +0200

    ipa: ipu3: fix coverity issues in AWB
    The BNR configuration structure has inconsistent initialisation of two
    members.
    
    This was picked up by Coverity, after merge.
    While this code is still functional, fix them to be consistent with the
    other initialisations within the struct.
    
    Reported-by: Coverity CID=320056
    Reported-by: Coverity CID=320058
    Signed-off-by: Jean-Michel Hautbois <jeanmichel.hautbois@ideasonboard.com>
    Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
    Tested-by: Niklas Söderlund <niklas.soderlund@ragnatech.se>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/ipa/ipu3/ipu3_awb.cpp
+++ src/ipa/ipu3/ipu3_awb.cpp
@@ -96,14 +96,14 @@ static const struct ipu3_uapi_bnr_static_config imguCssBnrDefaults = {
 	.wb_gains_thr = { 255, 255, 255, 255 },
 	.thr_coeffs = { 1700, 0, 31, 31, 0, 16 },
 	.thr_ctrl_shd = { 26, 26, 26, 26 },
-	.opt_center{ -648, 0, -366, 0 },
+	.opt_center = { -648, 0, -366, 0 },
 	.lut = {
 		{ 17, 23, 28, 32, 36, 39, 42, 45,
 		  48, 51, 53, 55, 58, 60, 62, 64,
 		  66, 68, 70, 72, 73, 75, 77, 78,
 		  80, 82, 83, 85, 86, 88, 89, 90 } },
 	.bp_ctrl = { 20, 0, 1, 40, 0, 6, 0, 6, 0 },
-	.dn_detect_ctrl{ 9, 3, 4, 0, 8, 0, 1, 1, 1, 1, 0 },
+	.dn_detect_ctrl = { 9, 3, 4, 0, 8, 0, 1, 1, 1, 1, 0 },
 	.column_size = 1296,
 	.opt_center_sqr = { 419904, 133956 },
 };

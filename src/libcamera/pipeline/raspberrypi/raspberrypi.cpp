raspberrypi/libcamera

commit d372aaa10ddb0ab405c4054a0a407921127f2100

pipeline: raspberrypi: Simplify RPiCameraData::clearIncompleteRequests()
With the addition of FrameBuffer::cancel(), the logic to clear and return
pending requests can be simplified by not having to queue all the request
buffers to the device before calling streamOff().

Signed-off-by: Naushir Patuck <naush@raspberrypi.com>
Tested-by: David Plowman <david.plowman@raspberrypi.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Reviewed-by: Jacopo Mondi <jacopo@jmondi.org>
Signed-off-by: Jacopo Mondi <jacopo@jmondi.org>

src/libcamera/pipeline/raspberrypi/raspberrypi.cpp
@@ -884,7 +884,9 @@ void PipelineHandlerRPi::stop(Camera *camera)
 	/* Disable SOF event generation. */
 	data->unicam_[Unicam::Image].dev()->setFrameStartEnabled(false);
 
-	/* This also stops the streams. */
+	for (auto const stream : data->streams_)
+		stream->dev()->streamOff();
+
 	data->clearIncompleteRequests();
 	data->bayerQueue_ = {};
 	data->embeddedQueue_ = {};
@@ -1478,48 +1480,16 @@ void RPiCameraData::ispOutputDequeue(FrameBuffer *buffer)
 void RPiCameraData::clearIncompleteRequests()
 {
 	/*
-	 * Queue up any buffers passed in the request.
-	 * This is needed because streamOff() will then mark the buffers as
-	 * cancelled.
-	 */
-	for (auto const request : requestQueue_) {
-		for (auto const stream : streams_) {
-			if (!stream->isExternal())
-				continue;
-
-			FrameBuffer *buffer = request->findBuffer(stream);
-			if (buffer)
-				stream->queueBuffer(buffer);
-		}
-	}
-
-	/* Stop all streams. */
-	for (auto const stream : streams_)
-		stream->dev()->streamOff();
-
-	/*
 	 * All outstanding requests (and associated buffers) must be returned
-	 * back to the pipeline. The buffers would have been marked as
-	 * cancelled by the call to streamOff() earlier.
+	 * back to the application.
 	 */
 	while (!requestQueue_.empty()) {
 		Request *request = requestQueue_.front();
-		/*
-		 * A request could be partially complete,
-		 * i.e. we have returned some buffers, but still waiting
-		 * for others or waiting for metadata.
-		 */
-		for (auto const stream : streams_) {
-			if (!stream->isExternal())
-				continue;
 
-			FrameBuffer *buffer = request->findBuffer(stream);
-			/*
-			 * Has the buffer already been handed back to the
-			 * request? If not, do so now.
-			 */
-			if (buffer && buffer->request())
-				pipe_->completeBuffer(request, buffer);
+		for (auto &b : request->buffers()) {
+			FrameBuffer *buffer = b.second;
+			buffer->cancel();
+			pipe_->completeBuffer(request, buffer);
 		}
 
 		pipe_->completeRequest(request);

commit e87fb20f8f5db9d54b9ca0bb0fdd8a1a9d30a8bd

pipeline: raspberrypi: Fix a bug when clearing out Request buffers on stop
When RPiCameraData::clearIncompleteRequests() clears out the request
queue during a stop condition, it unconditionally calls completeBuffer()
on all buffers in each request.  This is wrong, as a buffer could have
already been completed as part of the current request, but the request
itself may not yet have completed.

Fix this by checking if the buffers in the request have been completed
before cancelling them.

Fixes: d372aaa10ddb ("pipeline: raspberrypi: Simplify RPiCameraData::clearIncompleteRequests()")
Signed-off-by: Naushir Patuck <naush@raspberrypi.com>
Tested-by: David Plowman <david.plowman@raspberrypi.com>
Reviewed-by: David Plowman <david.plowman@raspberrypi.com>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

src/libcamera/pipeline/raspberrypi/raspberrypi.cpp
@@ -1520,8 +1520,14 @@ void RPiCameraData::clearIncompleteRequests()
 
 		for (auto &b : request->buffers()) {
 			FrameBuffer *buffer = b.second;
-			buffer->cancel();
-			pipe_->completeBuffer(request, buffer);
+			/*
+			 * Has the buffer already been handed back to the
+			 * request? If not, do so now.
+			 */
+			if (buffer->request()) {
+				buffer->cancel();
+				pipe_->completeBuffer(request, buffer);
+			}
 		}
 
 		pipe_->completeRequest(request);
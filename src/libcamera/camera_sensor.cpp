raspberrypi/libcamera

commit ad2bcbe5b55c8824288cd1752649f39f3a53af38

libcamera: camera_lens: add CameraLens as a member of CameraSensor
Add CameraLens as a member of CameraSensor. The patch does not implement how
to link the lens to the specific sensor yet. Only to provide an interface for
pipeline handler's usage.

Signed-off-by: Han-Lin Chen <hanlinchen@chromium.org>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Reviewed-by: Umang Jain <umang.jain@ideasonboard.com>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/libcamera/camera_sensor.cpp
@@ -20,6 +20,7 @@
 #include <libcamera/base/utils.h>
 
 #include "libcamera/internal/bayer_format.h"
+#include "libcamera/internal/camera_lens.h"
 #include "libcamera/internal/camera_sensor_properties.h"
 #include "libcamera/internal/formats.h"
 #include "libcamera/internal/sysfs.h"
@@ -786,6 +787,14 @@ void CameraSensor::updateControlInfo()
 {
 	subdev_->updateControlInfo();
 }
+
+/**
+ * \fn CameraSensor::focusLens_()
+ * \brief Retrieve the focus lens controller
+ *
+ * \return The focus lens controller. nullptr if no focus lens controller is
+ * connected to the sensor
+ */
 
 std::string CameraSensor::logPrefix() const
 {

include/libcamera/internal/camera_sensor.h
@@ -24,6 +24,7 @@
 namespace libcamera {
 
 class BayerFormat;
+class CameraLens;
 class MediaEntity;
 
 class CameraSensor : protected Loggable
@@ -60,6 +61,8 @@ public:
 
 	void updateControlInfo();
 
+	CameraLens *focusLens() { return focusLens_.get(); }
+
 protected:
 	std::string logPrefix() const override;
 
@@ -91,6 +94,8 @@ private:
 	const BayerFormat *bayerFormat_;
 
 	ControlList properties_;
+
+	std::unique_ptr<CameraLens> focusLens_;
 };
 
 } /* namespace libcamera */

commit 4b24b0bfe6dd1ec716a605b683e45cd47a0bd6ee

libcamera: camera_sensor: Fix doxygen warning
Commit ad2bcbe5b55c ("libcamera: camera_lens: add CameraLens as a member
of CameraSensor") incorrectly named the focusLens() function
"focusLens_" in the documentation, causing a doxygen warning. Fix it.

Fixes: ad2bcbe5b55c ("libcamera: camera_lens: add CameraLens as a member of CameraSensor")
Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Umang Jain <umang.jain@ideasonboard.com>
Reviewed-by: Jean-Michel Hautbois <jeanmichel.hautbois@ideasonboard.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/libcamera/camera_sensor.cpp
@@ -789,7 +789,7 @@ void CameraSensor::updateControlInfo()
 }
 
 /**
- * \fn CameraSensor::focusLens_()
+ * \fn CameraSensor::focusLens()
  * \brief Retrieve the focus lens controller
  *
  * \return The focus lens controller. nullptr if no focus lens controller is
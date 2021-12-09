350d4a4dbbec610e66d74b112d6cc271c82a4d9c
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2021-12-09 10:03:19 +0200

    Add Python bindings
    Add libcamera Python bindings. pybind11 is used to generate the C++ <->
    Python layer.
    
    Only a subset of libcamera classes are exposed. Implementing and testing
    the wrapper classes is challenging, and as such only classes that I have
    needed have been added so far.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>

---
+++ src/py/libcamera/__init__.py
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+# Copyright (C) 2021, Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
+
+from ._libcamera import *
+import mmap
+
+def __FrameBuffer__mmap(self, plane):
+	return mmap.mmap(self.fd(plane), self.length(plane), mmap.MAP_SHARED, mmap.PROT_READ)
+
+FrameBuffer.mmap = __FrameBuffer__mmap

d4453a6413d4f9da61bb5d552e4f530af8ab6769
Naushir Patuck <naush@raspberrypi.com>
2022-03-11 12:52:08 +0000

    Add lux algorithm.

---
+++ src/ipa/raspberrypi/pisp/controller/rpi/lux.cpp
@@ -0,0 +1,108 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (C) 2019, Raspberry Pi (Trading) Limited
+ *
+ * lux.cpp - Lux control algorithm
+ */
+#include <math.h>
+
+#include <linux/bcm2835-isp.h>
+
+#include <libcamera/base/log.h>
+
+#include "device_status.h"
+
+#include "lux.hpp"
+
+using namespace RPiController;
+using namespace libcamera;
+using namespace std::literals::chrono_literals;
+
+LOG_DEFINE_CATEGORY(RPiLux)
+
+#define NAME "rpi.lux"
+
+Lux::Lux(Controller *controller)
+	: PiSPAlgorithm(controller)
+{
+	// Put in some defaults as there will be no meaningful values until
+	// Process has run.
+	status_.aperture = 1.0;
+	status_.lux = 400;
+}
+
+char const *Lux::Name() const
+{
+	return NAME;
+}
+
+void Lux::Read(boost::property_tree::ptree const &params)
+{
+	reference_shutter_speed_ =
+		params.get<double>("reference_shutter_speed") * 1.0us;
+	reference_gain_ = params.get<double>("reference_gain");
+	reference_aperture_ = params.get<double>("reference_aperture", 1.0);
+	reference_Y_ = params.get<double>("reference_Y");
+	reference_lux_ = params.get<double>("reference_lux");
+	current_aperture_ = reference_aperture_;
+}
+
+void Lux::SetCurrentAperture(double aperture)
+{
+	current_aperture_ = aperture;
+}
+
+void Lux::Prepare([[maybe_unused]] PiSPStatisticsPtr &stats, Metadata *image_metadata)
+{
+	std::unique_lock<std::mutex> lock(mutex_);
+	image_metadata->Set("lux.status", status_);
+}
+
+#if 0
+void Lux::Process(VC4StatisticsPtr &stats, Metadata *image_metadata)
+{
+	DeviceStatus device_status;
+	if (image_metadata->Get("device.status", device_status) == 0) {
+		double current_gain = device_status.analogue_gain;
+		double current_aperture = device_status.aperture;
+		if (current_aperture == 0)
+			current_aperture = current_aperture_;
+		uint64_t sum = 0;
+		uint32_t num = 0;
+		uint32_t *bin = stats->hist[0].g_hist;
+		const int num_bins = sizeof(stats->hist[0].g_hist) /
+				     sizeof(stats->hist[0].g_hist[0]);
+		for (int i = 0; i < num_bins; i++)
+			sum += bin[i] * (uint64_t)i, num += bin[i];
+		// add .5 to reflect the mid-points of bins
+		double current_Y = sum / (double)num + .5;
+		double gain_ratio = reference_gain_ / current_gain;
+		double shutter_speed_ratio =
+			reference_shutter_speed_ / device_status.shutter_speed;
+		double aperture_ratio = reference_aperture_ / current_aperture;
+		double Y_ratio = current_Y * (65536 / num_bins) / reference_Y_;
+		double estimated_lux = shutter_speed_ratio * gain_ratio *
+				       aperture_ratio * aperture_ratio *
+				       Y_ratio * reference_lux_;
+		LuxStatus status;
+		status.lux = estimated_lux;
+		status.aperture = current_aperture;
+		LOG(RPiLux, Debug) << ": estimated lux " << estimated_lux;
+		{
+			std::unique_lock<std::mutex> lock(mutex_);
+			status_ = status;
+		}
+		// Overwrite the metadata here as well, so that downstream
+		// algorithms get the latest value.
+		image_metadata->Set("lux.status", status);
+	} else
+		LOG(RPiLux, Warning) << ": no device metadata";
+}
+#endif
+
+// Register algorithm with the system.
+static Algorithm *Create(Controller *controller)
+{
+	return (Algorithm *)new Lux(controller);
+}
+static RegisterAlgorithm reg(NAME, &Create);
---
+++ src/ipa/raspberrypi/pisp/controller/rpi/lux.hpp
@@ -0,0 +1,42 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (C) 2019, Raspberry Pi (Trading) Limited
+ *
+ * lux.hpp - Lux control algorithm
+ */
+#pragma once
+
+#include <mutex>
+
+#include <libcamera/base/utils.h>
+
+#include "../lux_status.h"
+#include "algorithm.hpp"
+
+// This is our implementation of the "lux control algorithm".
+
+namespace RPiController {
+
+class Lux : public PiSPAlgorithm
+{
+public:
+	Lux(Controller *controller);
+	char const *Name() const override;
+	void Read(boost::property_tree::ptree const &params) override;
+	void Prepare(PiSPStatisticsPtr &stats, Metadata *image_metadata) override;
+	void SetCurrentAperture(double aperture);
+
+private:
+	// These values define the conditions of the reference image, against
+	// which we compare the new image.
+	libcamera::utils::Duration reference_shutter_speed_;
+	double reference_gain_;
+	double reference_aperture_; // units of 1/f
+	double reference_Y_; // out of 65536
+	double reference_lux_;
+	double current_aperture_;
+	LuxStatus status_;
+	std::mutex mutex_;
+};
+
+} // namespace RPiController

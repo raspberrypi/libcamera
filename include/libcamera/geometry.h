raspberrypi/libcamera

commit c730dc74795d6a603ac9f725c081ff07ed70ee35

libcamera: geometry: Add operator<< for classes in geometry
Add operator<< for geometry classes for easier logging.

Signed-off-by: Han-Lin Chen <hanlinchen@chromium.org>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Paul Elder <paul.elder@ideasonboard.com>
Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

include/libcamera/geometry.h
@@ -46,6 +46,8 @@ static inline bool operator!=(const Point &lhs, const Point &rhs)
 	return !(lhs == rhs);
 }
 
+std::ostream &operator<<(std::ostream &out, const Point &p);
+
 class Size
 {
 public:
@@ -192,6 +194,8 @@ static inline bool operator>=(const Size &lhs, const Size &rhs)
 	return !(lhs < rhs);
 }
 
+std::ostream &operator<<(std::ostream &out, const Size &s);
+
 class SizeRange
 {
 public:
@@ -231,6 +235,8 @@ static inline bool operator!=(const SizeRange &lhs, const SizeRange &rhs)
 {
 	return !(lhs == rhs);
 }
+
+std::ostream &operator<<(std::ostream &out, const SizeRange &sr);
 
 class Rectangle
 {
@@ -290,5 +296,7 @@ static inline bool operator!=(const Rectangle &lhs, const Rectangle &rhs)
 {
 	return !(lhs == rhs);
 }
+
+std::ostream &operator<<(std::ostream &out, const Rectangle &r);
 
 } /* namespace libcamera */

src/libcamera/geometry.cpp
@@ -56,8 +56,7 @@ namespace libcamera {
 const std::string Point::toString() const
 {
 	std::stringstream ss;
-
-	ss << "(" << x << "," << y << ")";
+	ss << *this;
 
 	return ss.str();
 }
@@ -82,6 +81,18 @@ bool operator==(const Point &lhs, const Point &rhs)
  * \brief Compare points for inequality
  * \return True if the two points are not equal, false otherwise
  */
+
+/**
+ * \brief Insert a text representation of a Point into an output stream
+ * \param[in] out The output stream
+ * \param[in] p The point
+ * \return The output stream \a out
+ */
+std::ostream &operator<<(std::ostream &out, const Point &p)
+{
+	out << "(" << p.x << ", " << p.y << ")";
+	return out;
+}
 
 /**
  * \struct Size
@@ -124,7 +135,10 @@ bool operator==(const Point &lhs, const Point &rhs)
  */
 const std::string Size::toString() const
 {
-	return std::to_string(width) + "x" + std::to_string(height);
+	std::stringstream ss;
+	ss << *this;
+
+	return ss.str();
 }
 
 /**
@@ -429,6 +443,18 @@ bool operator<(const Size &lhs, const Size &rhs)
  */
 
 /**
+ * \brief Insert a text representation of a Size into an output stream
+ * \param[in] out The output stream
+ * \param[in] s The size
+ * \return The output stream \a out
+ */
+std::ostream &operator<<(std::ostream &out, const Size &s)
+{
+	out << s.width << "x" << s.height;
+	return out;
+}
+
+/**
  * \struct SizeRange
  * \brief Describe a range of sizes
  *
@@ -528,9 +554,7 @@ bool SizeRange::contains(const Size &size) const
 std::string SizeRange::toString() const
 {
 	std::stringstream ss;
-
-	ss << "(" << min.toString() << ")-(" << max.toString() << ")/(+"
-	   << hStep << ",+" << vStep << ")";
+	ss << *this;
 
 	return ss.str();
 }
@@ -549,6 +573,20 @@ bool operator==(const SizeRange &lhs, const SizeRange &rhs)
  * \brief Compare size ranges for inequality
  * \return True if the two size ranges are not equal, false otherwise
  */
+
+/**
+ * \brief Insert a text representation of a SizeRange into an output stream
+ * \param[in] out The output stream
+ * \param[in] sr The size range
+ * \return The output stream \a out
+ */
+std::ostream &operator<<(std::ostream &out, const SizeRange &sr)
+{
+	out << "(" << sr.min << ")-(" << sr.max << ")/(+"
+	    << sr.hStep << ",+" << sr.vStep << ")";
+
+	return out;
+}
 
 /**
  * \struct Rectangle
@@ -624,8 +662,7 @@ bool operator==(const SizeRange &lhs, const SizeRange &rhs)
 const std::string Rectangle::toString() const
 {
 	std::stringstream ss;
-
-	ss << "(" << x << "x" << y << ")/" << width << "x" << height;
+	ss << *this;
 
 	return ss.str();
 }
@@ -795,5 +832,17 @@ bool operator==(const Rectangle &lhs, const Rectangle &rhs)
  * \brief Compare rectangles for inequality
  * \return True if the two rectangles are not equal, false otherwise
  */
+
+/**
+ * \brief Insert a text representation of a Rectangle into an output stream
+ * \param[in] out The output stream
+ * \param[in] r The rectangle
+ * \return The output stream \a out
+ */
+std::ostream &operator<<(std::ostream &out, const Rectangle &r)
+{
+	out << "(" << r.x << "x" << r.y << ")/" << r.width << "x" << r.height;
+	return out;
+}
 
 } /* namespace libcamera */

commit e8f35e5f33597bb40a994df3eb836955e95d5095

libcamera: geometry: Add missing ostream header in geometry.h
Commit c730dc74795d ("libcamera: geometry: Add operator<< for classes in
geometry") added usage of std::ostream in geometry.h but forget to
include the corresponding header. Fix it to avoid future compilation
breakages if indirect inclusions are changed.

Fixes: c730dc74795d ("libcamera: geometry: Add operator<< for classes in geometry")
Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Reviewed-by: Jacopo Mondi <jacopo@jmondi.org>

include/libcamera/geometry.h
@@ -8,6 +8,7 @@
 #pragma once
 
 #include <algorithm>
+#include <ostream>
 #include <string>
 
 #include <libcamera/base/compiler.h>
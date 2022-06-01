raspberrypi/libcamera

commit 8a845ab078c3fe12ac4edd16c8cbac5b7ec03b98

libcamera: Replace toString with operator<<() for format classes
Now that format classes implement the stream formatting operator<<(),
use it instead of the toString() function.

Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Reviewed-by: Jacopo Mondi <jacopo@jmondi.org>

src/libcamera/pipeline/ipu3/cio2.cpp
@@ -211,7 +211,7 @@ int CIO2Device::configure(const Size &size, V4L2DeviceFormat *outputFormat)
 	if (ret)
 		return ret;
 
-	LOG(IPU3, Debug) << "CIO2 output format " << outputFormat->toString();
+	LOG(IPU3, Debug) << "CIO2 output format " << outputFormat;
 
 	return 0;
 }

src/libcamera/pipeline/ipu3/imgu.cpp
@@ -479,7 +479,7 @@ int ImgUDevice::configure(const PipeConfig &pipeConfig, V4L2DeviceFormat *inputF
 	if (ret)
 		return ret;
 
-	LOG(IPU3, Debug) << "ImgU input format = " << inputFormat->toString();
+	LOG(IPU3, Debug) << "ImgU input format = " << inputFormat;
 
 	/*
 	 * \todo The IPU3 driver implementation shall be changed to use the
@@ -511,7 +511,7 @@ int ImgUDevice::configure(const PipeConfig &pipeConfig, V4L2DeviceFormat *inputF
 	if (ret)
 		return ret;
 
-	LOG(IPU3, Debug) << "ImgU GDC format = " << gdcFormat.toString();
+	LOG(IPU3, Debug) << "ImgU GDC format = " << gdcFormat;
 
 	StreamConfiguration paramCfg = {};
 	paramCfg.size = inputFormat->size;
@@ -568,7 +568,7 @@ int ImgUDevice::configureVideoDevice(V4L2VideoDevice *dev, unsigned int pad,
 
 	const char *name = dev == output_.get() ? "output" : "viewfinder";
 	LOG(IPU3, Debug) << "ImgU " << name << " format = "
-			 << outputFormat->toString();
+			 << outputFormat;
 
 	return 0;
 }

src/libcamera/pipeline/simple/simple.cpp
@@ -607,10 +607,10 @@ int SimpleCameraData::setupFormats(V4L2SubdeviceFormat *format,
 				LOG(SimplePipeline, Debug)
 					<< "Source '" << source->entity()->name()
 					<< "':" << source->index()
-					<< " produces " << sourceFormat.toString()
+					<< " produces " << sourceFormat
 					<< ", sink '" << sink->entity()->name()
 					<< "':" << sink->index()
-					<< " requires " << format->toString();
+					<< " requires " << format;
 				return -EINVAL;
 			}
 		}
@@ -620,7 +620,7 @@ int SimpleCameraData::setupFormats(V4L2SubdeviceFormat *format,
 			<< "':" << source->index()
 			<< " -> '" << sink->entity()->name()
 			<< "':" << sink->index()
-			<< " configured with format " << format->toString();
+			<< " configured with format " << format;
 	}
 
 	return 0;
@@ -939,8 +939,7 @@ int SimplePipelineHandler::configure(Camera *camera, CameraConfiguration *c)
 	    captureFormat.size != pipeConfig->captureSize) {
 		LOG(SimplePipeline, Error)
 			<< "Unable to configure capture in "
-			<< pipeConfig->captureSize << "-"
-			<< videoFormat.toString();
+			<< pipeConfig->captureSize << "-" << videoFormat;
 		return -EINVAL;
 	}
 

src/android/camera_capabilities.cpp
@@ -554,7 +554,7 @@ int CameraCapabilities::initializeStreamConfigurations()
 			formatsMap_[androidFormat] = formats::MJPEG;
 			LOG(HAL, Debug) << "Mapped Android format "
 					<< camera3Format.name << " to "
-					<< formats::MJPEG.toString()
+					<< formats::MJPEG
 					<< " (fixed mapping)";
 			continue;
 		}
@@ -566,7 +566,7 @@ int CameraCapabilities::initializeStreamConfigurations()
 		PixelFormat mappedFormat;
 		for (const PixelFormat &pixelFormat : libcameraFormats) {
 
-			LOG(HAL, Debug) << "Testing " << pixelFormat.toString();
+			LOG(HAL, Debug) << "Testing " << pixelFormat;
 
 			/*
 			 * The stream configuration size can be adjusted,
@@ -605,7 +605,7 @@ int CameraCapabilities::initializeStreamConfigurations()
 		formatsMap_[androidFormat] = mappedFormat;
 		LOG(HAL, Debug) << "Mapped Android format "
 				<< camera3Format.name << " to "
-				<< mappedFormat.toString();
+				<< mappedFormat;
 
 		std::vector<Size> resolutions;
 		const PixelFormatInfo &info = PixelFormatInfo::info(mappedFormat);

src/android/camera_device.cpp
@@ -575,7 +575,7 @@ int CameraDevice::configureStreams(camera3_stream_configuration_t *stream_list)
 			       << ", crop_rotate_scale_degrees: "
 			       << rotationToString(stream->crop_rotate_scale_degrees)
 #endif
-			       << " (" << format.toString() << ")";
+			       << " (" << format << ")";
 
 		if (!format.isValid())
 			return -EINVAL;
@@ -926,7 +926,7 @@ int CameraDevice::processCaptureRequest(camera3_capture_request_t *camera3Reques
 		   << camera3Stream->height << ")"
 		   << "[" << utils::hex(camera3Stream->format) << "] -> "
 		   << "(" << cameraStream->configuration().size << ")["
-		   << cameraStream->configuration().pixelFormat.toString() << "]";
+		   << cameraStream->configuration().pixelFormat << "]";
 
 		/*
 		 * Inspect the camera stream type, create buffers opportunely

src/android/jpeg/encoder_libjpeg.cpp
@@ -59,7 +59,7 @@ const struct JPEGPixelFormatInfo &findPixelInfo(const PixelFormat &format)
 	const auto iter = pixelInfo.find(format);
 	if (iter == pixelInfo.end()) {
 		LOG(JPEG, Error) << "Unsupported pixel format for JPEG encoder: "
-				 << format.toString();
+				 << format;
 		return invalidPixelFormat;
 	}
 

src/android/jpeg/thumbnailer.cpp
@@ -30,7 +30,7 @@ void Thumbnailer::configure(const Size &sourceSize, PixelFormat pixelFormat)
 	if (pixelFormat_ != formats::NV12) {
 		LOG(Thumbnailer, Error)
 			<< "Failed to configure: Pixel Format "
-			<< pixelFormat_.toString() << " unsupported.";
+			<< pixelFormat_ << " unsupported.";
 		return;
 	}
 

src/android/mm/generic_camera_buffer.cpp
@@ -66,8 +66,7 @@ CameraBuffer::Private::Private([[maybe_unused]] CameraBuffer *cameraBuffer,
 	const auto &info = PixelFormatInfo::info(pixelFormat);
 	if (!info.isValid()) {
 		error_ = -EINVAL;
-		LOG(HAL, Error) << "Invalid pixel format: "
-				<< pixelFormat.toString();
+		LOG(HAL, Error) << "Invalid pixel format: " << pixelFormat;
 		return;
 	}
 

commit 2094ac4f8cf7c437d4ce8e35eef749f24f6f551e

libcamera: Fix incorrect toString() to operator<<() conversions
Commit 8a845ab078c3 ("libcamera: Replace toString with operator<<() for
format classes") incorrectly converted some of the toString() usages,
resulting in pointer values being printed instead of formats. Fix it.

Fixes: 8a845ab078c3 ("libcamera: Replace toString with operator<<() for format classes")
Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Paul Elder <paul.elder@ideasonboard.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/libcamera/pipeline/ipu3/cio2.cpp
@@ -211,7 +211,7 @@ int CIO2Device::configure(const Size &size, V4L2DeviceFormat *outputFormat)
 	if (ret)
 		return ret;
 
-	LOG(IPU3, Debug) << "CIO2 output format " << outputFormat;
+	LOG(IPU3, Debug) << "CIO2 output format " << *outputFormat;
 
 	return 0;
 }

src/libcamera/pipeline/ipu3/imgu.cpp
@@ -479,7 +479,7 @@ int ImgUDevice::configure(const PipeConfig &pipeConfig, V4L2DeviceFormat *inputF
 	if (ret)
 		return ret;
 
-	LOG(IPU3, Debug) << "ImgU input format = " << inputFormat;
+	LOG(IPU3, Debug) << "ImgU input format = " << *inputFormat;
 
 	/*
 	 * \todo The IPU3 driver implementation shall be changed to use the
@@ -568,7 +568,7 @@ int ImgUDevice::configureVideoDevice(V4L2VideoDevice *dev, unsigned int pad,
 
 	const char *name = dev == output_.get() ? "output" : "viewfinder";
 	LOG(IPU3, Debug) << "ImgU " << name << " format = "
-			 << outputFormat;
+			 << *outputFormat;
 
 	return 0;
 }

src/libcamera/pipeline/simple/simple.cpp
@@ -610,7 +610,7 @@ int SimpleCameraData::setupFormats(V4L2SubdeviceFormat *format,
 					<< " produces " << sourceFormat
 					<< ", sink '" << sink->entity()->name()
 					<< "':" << sink->index()
-					<< " requires " << format;
+					<< " requires " << *format;
 				return -EINVAL;
 			}
 		}
@@ -620,7 +620,7 @@ int SimpleCameraData::setupFormats(V4L2SubdeviceFormat *format,
 			<< "':" << source->index()
 			<< " -> '" << sink->entity()->name()
 			<< "':" << sink->index()
-			<< " configured with format " << format;
+			<< " configured with format " << *format;
 	}
 
 	return 0;
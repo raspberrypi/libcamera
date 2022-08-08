raspberrypi/libcamera

commit 7b8df9fe6b3eedc46667d07ad2c90c89ca823e0c

cam: sdl_sink: Add NV12 texture support
Extend the SDL sink with support for NV12 textures, useful on platforms
that don't support packed YUYV formats.

Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Reviewed-by: Jacopo Mondi <jacopo@jmondi.org>
Reviewed-by: Eric Curtin <ecurtin@redhat.com>
Tested-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/cam/sdl_sink.cpp
@@ -67,6 +67,9 @@ int SDLSink::configure(const libcamera::CameraConfiguration &config)
 		texture_ = std::make_unique<SDLTextureMJPG>(rect_);
 		break;
 #endif
+	case libcamera::formats::NV12:
+		texture_ = std::make_unique<SDLTextureNV12>(rect_, cfg.stride);
+		break;
 	case libcamera::formats::YUYV:
 		texture_ = std::make_unique<SDLTextureYUYV>(rect_, cfg.stride);
 		break;
@@ -188,7 +191,7 @@ void SDLSink::renderBuffer(FrameBuffer *buffer)
 	std::vector<Span<const uint8_t>> planes;
 	unsigned int i = 0;
 
-	planes.reserve(buffer->metadata()->planes().size());
+	planes.reserve(buffer->metadata().planes().size());
 
 	for (const FrameMetadata::Plane &meta : buffer->metadata().planes()) {
 		Span<uint8_t> data = image->data(i);

src/cam/sdl_texture_yuv.cpp
@@ -2,12 +2,23 @@
 /*
  * Copyright (C) 2022, Ideas on Board Oy
  *
- * sdl_texture_yuv.cpp - SDL Texture YUYV
+ * sdl_texture_yuv.cpp - SDL YUV Textures
  */
 
 #include "sdl_texture_yuv.h"
 
 using namespace libcamera;
+
+SDLTextureNV12::SDLTextureNV12(const SDL_Rect &rect, unsigned int stride)
+	: SDLTexture(rect, SDL_PIXELFORMAT_NV12, stride)
+{
+}
+
+void SDLTextureNV12::update(const std::vector<libcamera::Span<const uint8_t>> &data)
+{
+	SDL_UpdateNVTexture(ptr_, &rect_, data[0].data(), pitch_,
+			    data[1].data(), pitch_);
+}
 
 SDLTextureYUYV::SDLTextureYUYV(const SDL_Rect &rect, unsigned int stride)
 	: SDLTexture(rect, SDL_PIXELFORMAT_YUY2, stride)

src/cam/sdl_texture_yuv.h
@@ -2,12 +2,19 @@
 /*
  * Copyright (C) 2022, Ideas on Board Oy
  *
- * sdl_texture_yuv.h - SDL Texture YUYV
+ * sdl_texture_yuv.h - SDL YUV Textures
  */
 
 #pragma once
 
 #include "sdl_texture.h"
+
+class SDLTextureNV12 : public SDLTexture
+{
+public:
+	SDLTextureNV12(const SDL_Rect &rect, unsigned int stride);
+	void update(const std::vector<libcamera::Span<const uint8_t>> &data) override;
+};
 
 class SDLTextureYUYV : public SDLTexture
 {

commit fe8941d7d61bd22ed66e5b5615e931c68fdf9bfa

cam: sdl_sink: Fix compilation with SDL2 <2.0.16
The SDL_UpdateNVTexture() function, used for NV12 texture support, has
been introduced in SDL2 2.0.16. Compiling against older SDL versions
fails with

../src/cam/sdl_texture_yuv.cpp: In member function ‘virtual void SDLTextureNV12::update(const std::vector<libcamera::Span<const unsigned char> >&)’:
../src/cam/sdl_texture_yuv.cpp:19:2: error: ‘SDL_UpdateNVTexture’ was not declared in this scope; did you mean ‘SDL_UpdateYUVTexture’?
   19 |  SDL_UpdateNVTexture(ptr_, &rect_, data[0].data(), pitch_,
      |  ^~~~~~~~~~~~~~~~~~~
      |  SDL_UpdateYUVTexture

Fix it with conditional compilation based on the SDL version.

Fixes: 7b8df9fe6b3e ("cam: sdl_sink: Add NV12 texture support")
Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Eric Curtin <ecurtin@redhat.com>

src/cam/sdl_sink.cpp
@@ -67,9 +67,11 @@ int SDLSink::configure(const libcamera::CameraConfiguration &config)
 		texture_ = std::make_unique<SDLTextureMJPG>(rect_);
 		break;
 #endif
+#if SDL_VERSION_ATLEAST(2, 0, 16)
 	case libcamera::formats::NV12:
 		texture_ = std::make_unique<SDLTextureNV12>(rect_, cfg.stride);
 		break;
+#endif
 	case libcamera::formats::YUYV:
 		texture_ = std::make_unique<SDLTextureYUYV>(rect_, cfg.stride);
 		break;

src/cam/sdl_texture_yuv.cpp
@@ -9,6 +9,7 @@
 
 using namespace libcamera;
 
+#if SDL_VERSION_ATLEAST(2, 0, 16)
 SDLTextureNV12::SDLTextureNV12(const SDL_Rect &rect, unsigned int stride)
 	: SDLTexture(rect, SDL_PIXELFORMAT_NV12, stride)
 {
@@ -19,6 +20,7 @@ void SDLTextureNV12::update(const std::vector<libcamera::Span<const uint8_t>> &d
 	SDL_UpdateNVTexture(ptr_, &rect_, data[0].data(), pitch_,
 			    data[1].data(), pitch_);
 }
+#endif
 
 SDLTextureYUYV::SDLTextureYUYV(const SDL_Rect &rect, unsigned int stride)
 	: SDLTexture(rect, SDL_PIXELFORMAT_YUY2, stride)

src/cam/sdl_texture_yuv.h
@@ -9,12 +9,14 @@
 
 #include "sdl_texture.h"
 
+#if SDL_VERSION_ATLEAST(2, 0, 16)
 class SDLTextureNV12 : public SDLTexture
 {
 public:
 	SDLTextureNV12(const SDL_Rect &rect, unsigned int stride);
 	void update(const std::vector<libcamera::Span<const uint8_t>> &data) override;
 };
+#endif
 
 class SDLTextureYUYV : public SDLTexture
 {
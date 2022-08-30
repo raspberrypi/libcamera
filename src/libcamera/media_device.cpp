raspberrypi/libcamera

commit b65feafe3244e6a4c794b73260a9dc790daa0f47

libcamera: media_device: Add functions to get a MediaLink
Add three overloaded functions 'link()' to retrieve a link between two
pads. Each overloaded implementation exposes a different method to
identify the source and sink pads.

Signed-off-by: Jacopo Mondi <jacopo@jmondi.org>

src/libcamera/media_device.cpp
@@ -307,6 +307,86 @@ MediaEntity *MediaDevice::getEntityByName(const std::string &name) const
 }
 
 /**
+ * \brief Retrieve the MediaLink connecting two pads, identified by entity
+ * names and pad indexes
+ * \param sourceName The source entity name
+ * \param sourceIdx The index of the source pad
+ * \param sinkName The sink entity name
+ * \param sinkIdx The index of the sink pad
+ *
+ * Find the link that connects the pads at index \a sourceIdx of the source
+ * entity with name \a sourceName, to the pad at index \a sinkIdx of the
+ * sink entity with name \a sinkName, if any.
+ *
+ * \sa MediaDevice::link(const MediaEntity *source, unsigned int sourceIdx, const MediaEntity *sink, unsigned int sinkIdx) const
+ * \sa MediaDevice::link(const MediaPad *source, const MediaPad *sink) const
+ *
+ * \return The link that connects the two pads, or nullptr if no such a link
+ * exists
+ */
+MediaLink *MediaDevice::link(const std::string &sourceName, unsigned int sourceIdx,
+			     const std::string &sinkName, unsigned int sinkIdx)
+{
+	const MediaEntity *source = getEntityByName(sourceName);
+	const MediaEntity *sink = getEntityByName(sinkName);
+	if (!source || !sink)
+		return nullptr;
+
+	return link(source, sourceIdx, sink, sinkIdx);
+}
+
+/**
+ * \brief Retrieve the MediaLink connecting two pads, identified by the
+ * entities they belong to and pad indexes
+ * \param source The source entity
+ * \param sourceIdx The index of the source pad
+ * \param sink The sink entity
+ * \param sinkIdx The index of the sink pad
+ *
+ * Find the link that connects the pads at index \a sourceIdx of the source
+ * entity \a source, to the pad at index \a sinkIdx of the sink entity \a
+ * sink, if any.
+ *
+ * \sa MediaDevice::link(const std::string &sourceName, unsigned int sourceIdx, const std::string &sinkName, unsigned int sinkIdx) const
+ * \sa MediaDevice::link(const MediaPad *source, const MediaPad *sink) const
+ *
+ * \return The link that connects the two pads, or nullptr if no such a link
+ * exists
+ */
+MediaLink *MediaDevice::link(const MediaEntity *source, unsigned int sourceIdx,
+			     const MediaEntity *sink, unsigned int sinkIdx)
+{
+
+	const MediaPad *sourcePad = source->getPadByIndex(sourceIdx);
+	const MediaPad *sinkPad = sink->getPadByIndex(sinkIdx);
+	if (!sourcePad || !sinkPad)
+		return nullptr;
+
+	return link(sourcePad, sinkPad);
+}
+
+/**
+ * \brief Retrieve the MediaLink that connects two pads
+ * \param source The source pad
+ * \param sink The sink pad
+ *
+ * \sa MediaDevice::link(const std::string &sourceName, unsigned int sourceIdx, const std::string &sinkName, unsigned int sinkIdx) const
+ * \sa MediaDevice::link(const MediaEntity *source, unsigned int sourceIdx, const MediaEntity *sink, unsigned int sinkIdx) const
+ *
+ * \return The link that connects the two pads, or nullptr if no such a link
+ * exists
+ */
+MediaLink *MediaDevice::link(const MediaPad *source, const MediaPad *sink)
+{
+	for (MediaLink *link : source->links()) {
+		if (link->sink()->id() == sink->id())
+			return link;
+	}
+
+	return nullptr;
+}
+
+/**
  * \var MediaDevice::objects_
  * \brief Global map of media objects (entities, pads, links) keyed by their
  * object id.

src/libcamera/include/media_device.h
@@ -40,6 +40,12 @@ public:
 	const std::vector<MediaEntity *> &entities() const { return entities_; }
 	MediaEntity *getEntityByName(const std::string &name) const;
 
+	MediaLink *link(const std::string &sourceName, unsigned int sourceIdx,
+			const std::string &sinkName, unsigned int sinkIdx);
+	MediaLink *link(const MediaEntity *source, unsigned int sourceIdx,
+			const MediaEntity *sink, unsigned int sinkIdx);
+	MediaLink *link(const MediaPad *source, const MediaPad *sink);
+
 private:
 	std::string driver_;
 	std::string devnode_;

commit 18764a15c7062e241df3ffb587bf44d0c14c898d

libcamera: media_device: Fix \sa tags for MediaDevice::link
doxygen didn't create proper links to the overloads of
MediaDevice::link because the signatures didn't match due to an
additional 'const'

While at it remove the unnecessary `MediaDevice::` and wrap the lines

Fixes: b65feafe3244 ("libcamera: media_device: Add functions to get a MediaLink")
Signed-off-by: Marvin Schmidt <marvin.schmidt1987@gmail.com>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Umang Jain <umang.jain@ideasonboard.com>
Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

src/libcamera/media_device.cpp
@@ -352,8 +352,9 @@ MediaEntity *MediaDevice::getEntityByName(const std::string &name) const
  * entity with name \a sourceName, to the pad at index \a sinkIdx of the
  * sink entity with name \a sinkName, if any.
  *
- * \sa MediaDevice::link(const MediaEntity *source, unsigned int sourceIdx, const MediaEntity *sink, unsigned int sinkIdx) const
- * \sa MediaDevice::link(const MediaPad *source, const MediaPad *sink) const
+ * \sa link(const MediaEntity *source, unsigned int sourceIdx,
+ *          const MediaEntity *sink, unsigned int sinkIdx)
+ * \sa link(const MediaPad *source, const MediaPad *sink)
  *
  * \return The link that connects the two pads, or nullptr if no such a link
  * exists
@@ -381,8 +382,9 @@ MediaLink *MediaDevice::link(const std::string &sourceName, unsigned int sourceI
  * entity \a source, to the pad at index \a sinkIdx of the sink entity \a
  * sink, if any.
  *
- * \sa MediaDevice::link(const std::string &sourceName, unsigned int sourceIdx, const std::string &sinkName, unsigned int sinkIdx) const
- * \sa MediaDevice::link(const MediaPad *source, const MediaPad *sink) const
+ * \sa link(const std::string &sourceName, unsigned int sourceIdx,
+ *          const std::string &sinkName, unsigned int sinkIdx)
+ * \sa link(const MediaPad *source, const MediaPad *sink)
  *
  * \return The link that connects the two pads, or nullptr if no such a link
  * exists
@@ -404,8 +406,10 @@ MediaLink *MediaDevice::link(const MediaEntity *source, unsigned int sourceIdx,
  * \param[in] source The source pad
  * \param[in] sink The sink pad
  *
- * \sa MediaDevice::link(const std::string &sourceName, unsigned int sourceIdx, const std::string &sinkName, unsigned int sinkIdx) const
- * \sa MediaDevice::link(const MediaEntity *source, unsigned int sourceIdx, const MediaEntity *sink, unsigned int sinkIdx) const
+ * \sa link(const std::string &sourceName, unsigned int sourceIdx,
+ *          const std::string &sinkName, unsigned int sinkIdx)
+ * \sa link(const MediaEntity *source, unsigned int sourceIdx,
+ *          const MediaEntity *sink, unsigned int sinkIdx)
  *
  * \return The link that connects the two pads, or nullptr if no such a link
  * exists
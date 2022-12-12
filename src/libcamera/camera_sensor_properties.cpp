raspberrypi/libcamera

commit e3b26b4c4eb2582ea778a38545a8ac7801384db2

libcamera: camera_sensor: Add IMX519 sensor properties
Add an entry for Arducam IMX519 sensor which has 1220x1220 pixel size
and supports four test pattern modes.

Signed-off-by: Umang Jain <umang.jain@ideasonboard.com>
Reviewed-by: Jacopo Mondi <jacopo@jmondi.org>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/libcamera/camera_sensor_properties.cpp
@@ -102,6 +102,16 @@ const CameraSensorProperties *CameraSensorProperties::get(const std::string &sen
 			.unitCellSize = { 1550, 1550 },
 			.testPatternModes = {},
 		} },
+		{ "imx519", {
+			.unitCellSize = { 1220, 1220 },
+			.testPatternModes = {
+				{ controls::draft::TestPatternModeOff, 0 },
+				{ controls::draft::TestPatternModeColorBars, 1 },
+				{ controls::draft::TestPatternModeSolidColor, 2 },
+				{ controls::draft::TestPatternModeColorBarsFadeToGray, 3 },
+				{ controls::draft::TestPatternModePn9, 4 },
+			},
+		} },
 		{ "ov2740", {
 			.unitCellSize = { 1400, 1400 },
 			.testPatternModes = {

commit b026e4a6a0a620ce994938c3d1babd3f3df4a47e

libcamera: camera_sensor: Rectify test patterns listed for IMX519
The IMX519 driver reports ColorBars and ColorBarsFadeToGray but upon
inspecting the test pattern modes via v4l2-ctl utility, these test
patterns do not comply with MIPI CCS v1.1 (Section 10.1).

Hence, omit from camera-sensor-properties database for now and add a
comment for the same.

Fixes: e3b26b4c4eb2 ("libcamera: camera_sensor: Add IMX519 sensor properties")
Signed-off-by: Umang Jain <umang.jain@ideasonboard.com>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/libcamera/camera_sensor_properties.cpp
@@ -106,10 +106,12 @@ const CameraSensorProperties *CameraSensorProperties::get(const std::string &sen
 			.unitCellSize = { 1220, 1220 },
 			.testPatternModes = {
 				{ controls::draft::TestPatternModeOff, 0 },
-				{ controls::draft::TestPatternModeColorBars, 1 },
 				{ controls::draft::TestPatternModeSolidColor, 2 },
-				{ controls::draft::TestPatternModeColorBarsFadeToGray, 3 },
 				{ controls::draft::TestPatternModePn9, 4 },
+				/*
+				 * The driver reports ColorBars and ColorBarsFadeToGray as well but
+				 * these two patterns do not comply with MIPI CCS v1.1 (Section 10.1).
+				 */
 			},
 		} },
 		{ "ov2740", {
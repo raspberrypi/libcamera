556cdb6a51fc3f7ff22cdbf5c5bb05fa70c0804a
Nick Hollinghurst <nick.hollinghurst@raspberrypi.com>
2023-01-23 15:49:26 +0000

    ipa: raspberrypi: Add autofocus algorithm interface headers
    Add a new AfAlgorithm class that defines the interface for the Raspberry Pi
    autofocus algorithm.
    
    Add an AfStatus structure that returns the results of the autofocus algorithm
    back to the IPA.
    
    Add a structure for PDAF statistics provided by supported sensors for use with
    an autofocus algorithm. This structure is currently based on the IMX708's PDAF
    "Type 1" statistics.
    
    Signed-off-by: Nick Hollinghurst <nick.hollinghurst@raspberrypi.com>
    Signed-off-by: Naushir Patuck <naush@raspberrypi.com>
    Reviewed-by: Naushir Patuck <naush@raspberrypi.com>
    Reviewed-by: David Plowman <david.plowman@raspberrypi.com>

---
+++ src/ipa/raspberrypi/controller/af_status.h
@@ -0,0 +1,35 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (C) 2022, Raspberry Pi Ltd
+ *
+ * af_status.h - AF control algorithm status
+ */
+#pragma once
+
+#include <optional>
+
+/*
+ * The AF algorithm should post the following structure into the image's
+ * "af.status" metadata. lensSetting should control the lens.
+ */
+
+enum class AfState {
+	Idle = 0,
+	Scanning,
+	Focused,
+	Failed
+};
+
+enum class AfPauseState {
+	Running = 0,
+	Pausing,
+	Paused
+};
+
+struct AfStatus {
+	/* state for reporting */
+	AfState state;
+	AfPauseState pauseState;
+	/* lensSetting should be sent to the lens driver, when valid */
+	std::optional<int> lensSetting;
+};

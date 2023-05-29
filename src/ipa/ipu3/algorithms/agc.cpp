db86713ec17978cd4d42b74cece5be34aaa6a693
Jacopo Mondi <jacopo.mondi@ideasonboard.com>
2023-05-29 14:39:25 +0200

    ipa: ipu3: agc: Drop hard-codec analogue gain max
    As the sensor's analogue gain range is known, drop the arbitrary
    maximum limit for the sensor analogue gain.
    
    Signed-off-by: Jacopo Mondi <jacopo.mondi@ideasonboard.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Daniel Scally <dan.scally@ideasonboard.com>
    Tested-by: Daniel Scally <dan.scally@ideasonboard.com>
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- src/ipa/ipu3/algorithms/agc.cpp
+++ src/ipa/ipu3/algorithms/agc.cpp
@@ -47,9 +47,8 @@ namespace ipa::ipu3::algorithms {
 
 LOG_DEFINE_CATEGORY(IPU3Agc)
 
-/* Limits for analogue gain values */
+/* Minimum limit for analogue gain value */
 static constexpr double kMinAnalogueGain = 1.0;
-static constexpr double kMaxAnalogueGain = 8.0;
 
 /* \todo Honour the FrameDurationLimits control instead of hardcoding a limit */
 static constexpr utils::Duration kMaxShutterSpeed = 60ms;
@@ -97,10 +96,10 @@ int Agc::configure(IPAContext &context,
 				    kMaxShutterSpeed);
 
 	minAnalogueGain_ = std::max(configuration.agc.minAnalogueGain, kMinAnalogueGain);
-	maxAnalogueGain_ = std::min(configuration.agc.maxAnalogueGain, kMaxAnalogueGain);
+	maxAnalogueGain_ = configuration.agc.maxAnalogueGain;
 
 	/* Configure the default exposure and gain. */
-	activeState.agc.gain = std::max(minAnalogueGain_, kMinAnalogueGain);
+	activeState.agc.gain = minAnalogueGain_;
 	activeState.agc.exposure = 10ms / configuration.sensor.lineDuration;
 
 	frameCount_ = 0;

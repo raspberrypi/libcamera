raspberrypi/libcamera

commit 0ee9339331c648232e87d2de2ccd5a92cc61cab2

ipa: rpi: agc: Gracefully handle missing agc modes
If a metering/exposure/constraint mode is not listed in the sensor
tuning file, and a control for the missing mode is set on the agc, we
terminate the application with a fatal log message.

Instead of this fatal termination, log a warning message and switch to
the appropriate default mode so that the application continues running.

Bug: https://github.com/raspberrypi/libcamera/issues/59
Bug: https://github.com/ayufan/camera-streamer/issues/67
Signed-off-by: Naushir Patuck <naush@raspberrypi.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/ipa/rpi/controller/rpi/agc.cpp
@@ -540,24 +540,39 @@ void Agc::housekeepConfig()
 	 */
 	if (meteringModeName_ != status_.meteringMode) {
 		auto it = config_.meteringModes.find(meteringModeName_);
-		if (it == config_.meteringModes.end())
-			LOG(RPiAgc, Fatal) << "No metering mode " << meteringModeName_;
-		meteringMode_ = &it->second;
+		if (it == config_.meteringModes.end()) {
+			LOG(RPiAgc, Warning) << "No metering mode " << meteringModeName_
+					     << ", defaulting to " << config_.defaultMeteringMode;
+			meteringModeName_ = config_.defaultMeteringMode;
+			meteringMode_ = &config_.meteringModes[meteringModeName_];
+		} else {
+			meteringMode_ = &it->second;
+		}
 		status_.meteringMode = meteringModeName_;
 	}
 	if (exposureModeName_ != status_.exposureMode) {
 		auto it = config_.exposureModes.find(exposureModeName_);
-		if (it == config_.exposureModes.end())
-			LOG(RPiAgc, Fatal) << "No exposure profile " << exposureModeName_;
-		exposureMode_ = &it->second;
+		if (it == config_.exposureModes.end()) {
+			LOG(RPiAgc, Warning) << "No exposure profile " << exposureModeName_
+					     << ", defaulting to " << config_.defaultExposureMode;
+			exposureModeName_ = config_.defaultExposureMode;
+			exposureMode_ = &config_.exposureModes[exposureModeName_];
+		} else {
+			exposureMode_ = &it->second;
+		}
 		status_.exposureMode = exposureModeName_;
 	}
 	if (constraintModeName_ != status_.constraintMode) {
 		auto it =
 			config_.constraintModes.find(constraintModeName_);
-		if (it == config_.constraintModes.end())
-			LOG(RPiAgc, Fatal) << "No constraint list " << constraintModeName_;
-		constraintMode_ = &it->second;
+		if (it == config_.constraintModes.end()) {
+			LOG(RPiAgc, Warning) << "No constraint list " << constraintModeName_
+					     << ", defaulting to " << config_.defaultConstraintMode;
+			constraintModeName_ = config_.defaultConstraintMode;
+			constraintMode_ = &config_.constraintModes[constraintModeName_];
+		} else {
+			constraintMode_ = &it->second;
+		}
 		status_.constraintMode = constraintModeName_;
 	}
 	LOG(RPiAgc, Debug) << "exposureMode "

commit 618b9aaa17713bed359112bda358e81e1eaea86c

ipa: rpi: agc: Do not switch to a default if a mode is unavailable
In commit 0ee9339331c6, a default metering/exposure/constraint mode is
used if a control sets a mode that is not listed in the camera tuning
file.

Setting a default mode may be undesirable in these cases, so instead
keep the agc mode unchanged. This also matches the behaviour for other
IPA controls where no changes are made in error conditions.

Fixes: 0ee9339331c6 ("ipa: rpi: agc: Gracefully handle missing agc modes")
Signed-off-by: Naushir Patuck <naush@raspberrypi.com>
Reviewed-by: David Plowman <david.plowman@raspberrypi.com>

src/ipa/rpi/controller/rpi/agc.cpp
@@ -541,39 +541,32 @@ void Agc::housekeepConfig()
 	if (meteringModeName_ != status_.meteringMode) {
 		auto it = config_.meteringModes.find(meteringModeName_);
 		if (it == config_.meteringModes.end()) {
-			LOG(RPiAgc, Warning) << "No metering mode " << meteringModeName_
-					     << ", defaulting to " << config_.defaultMeteringMode;
-			meteringModeName_ = config_.defaultMeteringMode;
-			meteringMode_ = &config_.meteringModes[meteringModeName_];
+			LOG(RPiAgc, Warning) << "No metering mode " << meteringModeName_;
+			meteringModeName_ = status_.meteringMode;
 		} else {
 			meteringMode_ = &it->second;
+			status_.meteringMode = meteringModeName_;
 		}
-		status_.meteringMode = meteringModeName_;
 	}
 	if (exposureModeName_ != status_.exposureMode) {
 		auto it = config_.exposureModes.find(exposureModeName_);
 		if (it == config_.exposureModes.end()) {
-			LOG(RPiAgc, Warning) << "No exposure profile " << exposureModeName_
-					     << ", defaulting to " << config_.defaultExposureMode;
-			exposureModeName_ = config_.defaultExposureMode;
-			exposureMode_ = &config_.exposureModes[exposureModeName_];
+			LOG(RPiAgc, Warning) << "No exposure profile " << exposureModeName_;
+			exposureModeName_ = status_.exposureMode;
 		} else {
 			exposureMode_ = &it->second;
+			status_.exposureMode = exposureModeName_;
 		}
-		status_.exposureMode = exposureModeName_;
 	}
 	if (constraintModeName_ != status_.constraintMode) {
-		auto it =
-			config_.constraintModes.find(constraintModeName_);
+		auto it = config_.constraintModes.find(constraintModeName_);
 		if (it == config_.constraintModes.end()) {
-			LOG(RPiAgc, Warning) << "No constraint list " << constraintModeName_
-					     << ", defaulting to " << config_.defaultConstraintMode;
-			constraintModeName_ = config_.defaultConstraintMode;
-			constraintMode_ = &config_.constraintModes[constraintModeName_];
+			LOG(RPiAgc, Warning) << "No constraint list " << constraintModeName_;
+			constraintModeName_ = status_.constraintMode;
 		} else {
 			constraintMode_ = &it->second;
+			status_.constraintMode = constraintModeName_;
 		}
-		status_.constraintMode = constraintModeName_;
 	}
 	LOG(RPiAgc, Debug) << "exposureMode "
 			   << exposureModeName_ << " constraintMode "
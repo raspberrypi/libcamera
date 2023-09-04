85614914855dc2758c319bbb971bfcafe9ea9b9c
Ben Benson <benbenson2004@gmail.com>
2023-09-04 10:41:49 +0100

    utils: raspberrypi: ctt: Changed CTT handling of VC4 and PiSP
    Changed how users select which platform to tune for. Now users
    specify a command line argument, '-t', to specify which target
    platform.
    
    Signed-off-by: Ben Benson <ben.benson@raspberrypi.com>

--- utils/raspberrypi/ctt/ctt_tools.py
+++ utils/raspberrypi/ctt/ctt_tools.py
@@ -65,11 +65,12 @@ def parse_input():
     directory = get_config(args_dict, '-i', None, 'string')
     config = get_config(args_dict, '-c', None, 'string')
     log_path = get_config(args_dict, '-l', None, 'string')
+    target = get_config(args_dict, '-t', "vc4", 'string')
     if directory is None:
         raise ArgError('\n\nERROR! No input directory given.')
     if json_output is None:
         raise ArgError('\n\nERROR! No output json given.')
-    return json_output, directory, config, log_path
+    return json_output, directory, config, log_path, target
 
 
 """

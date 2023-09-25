raspberrypi/libcamera

commit 3b9fe4ae996b532e6a3bfdcbd26b77514072e3f8

libcamera: formats: Add YUV444 and YVU444 pixel formats
Extend planar YUV format support with 4:4:4 formats. Those formats are
used by the i.MX8 ISI driver that will be supported by the simple
pipeline handler.

Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Jacopo Mondi <jacopo@jmondi.org>

src/libcamera/v4l2_pixelformat.cpp
@@ -117,6 +117,10 @@ const std::map<V4L2PixelFormat, V4L2PixelFormat::Info> vpf2pf{
 		{ formats::YUV422, "Planar YUV 4:2:2 (N-C)" } },
 	{ V4L2PixelFormat(V4L2_PIX_FMT_YVU422M),
 		{ formats::YVU422, "Planar YVU 4:2:2 (N-C)" } },
+	{ V4L2PixelFormat(V4L2_PIX_FMT_YUV444M),
+		{ formats::YUV444, "Planar YUV 4:4:4 (N-C)" } },
+	{ V4L2PixelFormat(V4L2_PIX_FMT_YUV444M),
+		{ formats::YVU444, "Planar YVU 4:4:4 (N-C)" } },
 
 	/* Greyscale formats. */
 	{ V4L2PixelFormat(V4L2_PIX_FMT_GREY),

src/libcamera/formats.cpp
@@ -495,6 +495,32 @@ const std::map<PixelFormat, PixelFormatInfo> pixelFormatInfo{
 		.pixelsPerGroup = 2,
 		.planes = {{ { 2, 1 }, { 1, 1 }, { 1, 1 } }},
 	} },
+	{ formats::YUV444, {
+		.name = "YUV444",
+		.format = formats::YUV444,
+		.v4l2Formats = {
+			.single = V4L2PixelFormat(),
+			.multi = V4L2PixelFormat(V4L2_PIX_FMT_YUV444M),
+		},
+		.bitsPerPixel = 24,
+		.colourEncoding = PixelFormatInfo::ColourEncodingYUV,
+		.packed = false,
+		.pixelsPerGroup = 1,
+		.planes = {{ { 1, 1 }, { 1, 1 }, { 1, 1 } }},
+	} },
+	{ formats::YVU444, {
+		.name = "YVU444",
+		.format = formats::YVU444,
+		.v4l2Formats = {
+			.single = V4L2PixelFormat(),
+			.multi = V4L2PixelFormat(V4L2_PIX_FMT_YVU444M),
+		},
+		.bitsPerPixel = 24,
+		.colourEncoding = PixelFormatInfo::ColourEncodingYUV,
+		.packed = false,
+		.pixelsPerGroup = 1,
+		.planes = {{ { 1, 1 }, { 1, 1 }, { 1, 1 } }},
+	} },
 
 	/* Greyscale formats. */
 	{ formats::R8, {

src/libcamera/formats.yaml
@@ -71,6 +71,10 @@ formats:
       fourcc: DRM_FORMAT_YUV422
   - YVU422:
       fourcc: DRM_FORMAT_YVU422
+  - YUV444:
+      fourcc: DRM_FORMAT_YUV444
+  - YVU444:
+      fourcc: DRM_FORMAT_YVU444
 
   - MJPEG:
       fourcc: DRM_FORMAT_MJPEG

commit 870943f80ab42764da532d185f6be44084611242

libcamera: formats: Fix typo in YV444 V4L2PixelFormat listing
This format was defined with the V4L2_PIX_FMT_YUV444M fourcc instead of
the correct V4L2_PIX_FMT_YVU444M fourcc.

Fixes: 3b9fe4ae996b ("libcamera: formats: Add YUV444 and YVU444 pixel formats")
Signed-off-by: Naushir Patuck <naush@raspberrypi.com>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/libcamera/v4l2_pixelformat.cpp
@@ -123,7 +123,7 @@ const std::map<V4L2PixelFormat, V4L2PixelFormat::Info> vpf2pf{
 		{ formats::YVU422, "Planar YVU 4:2:2 (N-C)" } },
 	{ V4L2PixelFormat(V4L2_PIX_FMT_YUV444M),
 		{ formats::YUV444, "Planar YUV 4:4:4 (N-C)" } },
-	{ V4L2PixelFormat(V4L2_PIX_FMT_YUV444M),
+	{ V4L2PixelFormat(V4L2_PIX_FMT_YVU444M),
 		{ formats::YVU444, "Planar YVU 4:4:4 (N-C)" } },
 
 	/* Greyscale formats. */
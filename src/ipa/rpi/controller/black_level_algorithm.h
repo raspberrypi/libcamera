430819e3b40b607a4e3856cb4a9e9945640d7bca
David Plowman <david.plowman@raspberrypi.com>
2023-12-05 16:49:10 +0000

    ipa: rpi: black_level: Add an initialValues method
    This allows the IPA to discover the correct black level values even
    before any frames have been processed. This is important on the PiSP
    platform where the front end black level blocks must be programmed in
    advance.
    
    Signed-off-by: David Plowman <david.plowman@raspberrypi.com>

---
+++ src/ipa/rpi/controller/black_level_algorithm.h
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (C) 2023, Raspberry Pi Ltd
+ *
+ * black_level_algorithm.h - black level control algorithm interface
+ */
+#pragma once
+
+#include "algorithm.h"
+
+namespace RPiController {
+
+class BlackLevelAlgorithm : public Algorithm
+{
+public:
+	BlackLevelAlgorithm(Controller *controller)
+		: Algorithm(controller) {}
+	/* A black level algorithm must provide the following: */
+	virtual void initialValues(uint16_t &blackLevelR, uint16_t &blackLevelG,
+				   uint16_t &blackLevelB) = 0;
+};
+
+} /* namespace RPiController */

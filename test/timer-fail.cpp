4f76beae8612f02e086e96d79447b7e8984c8821
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2024-01-19 03:21:15 +0200

    test: timer-thread: Move timer start from wrong thread to separate test
    Starting a timer from the wrong thread is expected to fail, and we test
    this in the timer-thread unit test. This is however not something that a
    caller is allowed to do, and libcamera will get assertion failures to
    catch this invalid usage. The unit test will then fail.
    
    To prepare for this, split the unit test in two, with a test that is
    expected by meson to succeed, and one that is expected to fail. The
    assertion will then cause an expected failure, making the test suite
    succeed.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Milan Zamazal <mzamazal@redhat.com>

--- test/timer-fail.cpp
+++ test/timer-fail.cpp
@@ -1,8 +1,8 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 /*
- * Copyright (C) 2019, Google Inc.
+ * Copyright (C) 2024, Ideas on Board Oy
  *
- * timer-thread.cpp - Threaded timer test
+ * timer-fail.cpp - Threaded timer failure test
  */
 
 #include <chrono>
@@ -26,6 +26,10 @@ public:
 		: timer_(this), timeout_(false)
 	{
 		timer_.timeout.connect(this, &TimeoutHandler::timeoutHandler);
+	}
+
+	void start()
+	{
 		timer_.start(100ms);
 	}
 
@@ -44,7 +48,7 @@ private:
 	bool timeout_;
 };
 
-class TimerThreadTest : public Test
+class TimerFailTest : public Test
 {
 protected:
 	int init()
@@ -58,17 +62,30 @@ protected:
 	int run()
 	{
 		/*
-		 * Test that the timer expires and emits the timeout signal in
-		 * the thread it belongs to.
+		 * Test that the forbidden operation of starting the timer from
+		 * another thread results in a failure. We need to interrupt the
+		 * event dispatcher to make sure we don't succeed simply because
+		 * the event dispatcher hasn't noticed the timer restart.
 		 */
+		timeout_.start();
+		thread_.eventDispatcher()->interrupt();
+
 		this_thread::sleep_for(chrono::milliseconds(200));
 
-		if (!timeout_.timeout()) {
-			cout << "Timer expiration test failed" << endl;
-			return TestFail;
+		/*
+		 * The wrong start() call should result in an assertion in debug
+		 * builds, and a timeout in release builds. The test is
+		 * therefore marked in meson.build as expected to fail. We need
+		 * to return TestPass in the unexpected (usually known as
+		 * "fail") case, and TestFail otherwise.
+		 */
+		if (timeout_.timeout()) {
+			cout << "Timer start from wrong thread succeeded unexpectedly"
+			     << endl;
+			return TestPass;
 		}
 
-		return TestPass;
+		return TestFail;
 	}
 
 	void cleanup()
@@ -83,4 +100,4 @@ private:
 	Thread thread_;
 };
 
-TEST_REGISTER(TimerThreadTest)
+TEST_REGISTER(TimerFailTest)

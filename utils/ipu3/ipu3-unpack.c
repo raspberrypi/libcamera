raspberrypi/libcamera

commit 23ac77dc4a096b92da7668e9ddfce10b22e73506

utils: ipu3: Add IPU3 raw capture unpack utility
The IPU3 captures Bayer data in a 25-pixels-in-32-bytes packed format,
which no standard tool can process. Add a quick implementation of data
unpacking to turn raw binary files into 16 bits per pixel unpacked Bayer
data.

Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Jacopo Mondi <jacopo@jmondi.org>

utils/ipu3/ipu3-unpack.c
@@ -1,0 +1,93 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * ipu3-unpack - Unpack IPU3 raw Bayer format to 16-bit Bayer
+ *
+ * Copyright 2018 Laurent Pinchart <laurent.pinchart@ideasonboard.com>
+ */
+#define _GNU_SOURCE
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+static void usage(const char *argv0)
+{
+	printf("Usage: %s input-file output-file\n", basename(argv0));
+	printf("Unpack the IPU3 raw Bayer format to 16-bit Bayer\n");
+}
+
+int main(int argc, char *argv[])
+{
+	int in_fd;
+	int out_fd;
+	int ret;
+
+	if (argc != 3) {
+		usage(argv[0]);
+		return 1;
+	}
+
+	in_fd = open(argv[1], O_RDONLY);
+	if (in_fd == -1) {
+		fprintf(stderr, "Failed to open input file '%s': %s\n",
+			argv[1], strerror(errno));
+		return 1;
+	}
+
+	out_fd = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0644);
+	if (out_fd == -1) {
+		fprintf(stderr, "Failed to open output file '%s': %s\n",
+			argv[2], strerror(errno));
+		return 1;
+	}
+
+	while (1) {
+		uint8_t in_data[32];
+		uint8_t out_data[50];
+		unsigned int i;
+
+		ret = read(in_fd, in_data, 32);
+		if (ret == -1) {
+			fprintf(stderr, "Failed to read input data: %s\n",
+				strerror(errno));
+			goto done;
+		}
+
+		if (ret < 32) {
+			if (ret != 0)
+				fprintf(stderr, "%u bytes of stray data at end of input\n",
+					ret);
+			break;
+		}
+
+		for (i = 0; i < 25; ++i) {
+			unsigned int index = (i * 10) / 8;
+			unsigned int lsb_shift = (i * 10) % 8;
+			unsigned int msb_shift = 8 - lsb_shift;
+			uint16_t pixel;
+
+			pixel = ((in_data[index+1] << msb_shift) & 0x3ff)
+			      | ((in_data[index+0] >> lsb_shift) & 0x3ff);
+			out_data[i*2+0] = (pixel >> 0) & 0xff;
+			out_data[i*2+1] = (pixel >> 8) & 0xff;
+		}
+
+		ret = write(out_fd, out_data, 50);
+		if (ret < -1) {
+			fprintf(stderr, "Failed to read input data: %s\n",
+				strerror(errno));
+			goto done;
+		}
+	}
+
+done:
+	close(in_fd);
+	close(out_fd);
+
+	return ret ? 1 : 0;
+}

meson.build
@@ -1,4 +1,4 @@
-project('libcamera - supporting complex camera pipelines', 'cpp',
+project('libcamera - supporting complex camera pipelines', 'c', 'cpp',
   version : '0.1',
   license : 'LGPL 2.1+')
 
@@ -6,6 +6,7 @@ inc = include_directories('include')
 
 subdir('lib')
 subdir('test')
+subdir('utils')
 
 pkg_mod = import('pkgconfig')
 pkg_mod.generate(libraries : libcamera,

utils/ipu3/meson.build
@@ -1,0 +1,1 @@
+ipu3_unpack = executable('ipu3-unpack', 'ipu3-unpack.c')

utils/meson.build
@@ -1,0 +1,1 @@
+subdir('ipu3')

commit bdd85fa01c6078950b93d4c9491694d077c3b60b

utils: ipu3: Fix return value check on file output
The data parsed by ipu3-unpack is written out using the write() c
library call, but the error code is incorrectly checked which misses the
single erroroneous return value returned by the function.

Fix it to explicitly check against the error code.

Fixes: 23ac77dc4a09 ("utils: ipu3: Add IPU3 raw capture unpack utility")
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Umang Jain <umang.jain@ideasonboard.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

utils/ipu3/ipu3-unpack.c
@@ -78,7 +78,7 @@ int main(int argc, char *argv[])
 		}
 
 		ret = write(out_fd, out_data, 50);
-		if (ret < -1) {
+		if (ret == -1) {
 			fprintf(stderr, "Failed to write output data: %s\n",
 				strerror(errno));
 			goto done;
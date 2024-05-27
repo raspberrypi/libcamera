raspberrypi/libcamera

commit 2031e2f2901403242329d7b59d97fa7c98758206

ipa: rpi: Add accessor function for monoSensor_
The monoSensor_ member variable will be used by the derived IPAs in an
upcoming commit, so add an accessor function for it.

Signed-off-by: Naushir Patuck <naush@raspberrypi.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Reviewed-by: Jacopo Mondi <jacopo.mondi@ideasonboard.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/ipa/rpi/common/ipa_base.h
@@ -49,6 +49,11 @@ public:
 	void processStats(const ProcessParams &params) override;
 
 protected:
+	bool monoSensor()
+	{
+		return monoSensor_;
+	}
+
 	/* Raspberry Pi controller specific defines. */
 	std::unique_ptr<RPiController::CamHelper> helper_;
 	RPiController::Controller controller_;

commit 807119d1e0d27ffe50b10ccfc6ee7bf64ea03e74

ipa: rpi: Make monoSensor() accessor const
The ipa::RPi::IpaBase::monoSensor() function doesn't modify the class.
Make it const.

Fixes: 2031e2f29014 ("ipa: rpi: Add accessor function for monoSensor_")
Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Reviewed-by: Jacopo Mondi <jacopo.mondi@ideasonboard.com>

src/ipa/rpi/common/ipa_base.h
@@ -49,7 +49,7 @@ public:
 	void processStats(const ProcessParams &params) override;
 
 protected:
-	bool monoSensor()
+	bool monoSensor() const
 	{
 		return monoSensor_;
 	}
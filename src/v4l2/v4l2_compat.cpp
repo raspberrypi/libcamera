1907c883cad5563dc082cd4624e8f0531e32ab9a
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2024-06-24 17:03:26 +0300

    v4l2: v4l2_compat: Include missing headers
    The close() and ioctl() functions are declared in the unistd.h and
    sys/ioctl.h headers. Include them to provide the declarations.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/v4l2/v4l2_compat.cpp
+++ src/v4l2/v4l2_compat.cpp
@@ -11,9 +11,11 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <stdarg.h>
+#include <sys/ioctl.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 #include <libcamera/base/utils.h>
 

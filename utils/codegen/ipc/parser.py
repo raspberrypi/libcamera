a5f12d2eb35a2fa57a4383869b5e616c3ce1e2ae
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2024-08-08 17:30:35 +0300

    meson: utils: Provide environment for Python scripts
    Python scripts run as part of the build process need to take a few
    actions specific to the environment in which they operate. One of those
    is disabling the Python bytecode cache, to avoid writing .pyc files to
    the source tree. This is done manually in the IPC generate.py and
    parser.py scripts.
    
    The current implementation is not ideal because it hardcodes in the
    scripts information related to the environment in which they operate. As
    those scripts are part of libcamera this is more of a theoretical issue
    than a practical one. A second issue is that future Python scripts will
    need to duplicate similar mechanisms, resulting in a higher maintenance
    burden.
    
    Address the issue with a different approach, by creating a meson
    environment for the Python scripts, and passing it to the
    custom_target() functions. The environment only disables the bytecode
    cache for now.
    
    The diffstat shows an increase in code size. This is expected to be
    offset by usage of the environment for more Python scripts, as well as
    support of more variables in the environment.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
    Reviewed-by: Daniel Scally <dan.scally@ideasonboard.com>
    Reviewed-by: Paul Elder <paul.elder@ideasonboard.com>

--- utils/codegen/ipc/parser.py
+++ utils/codegen/ipc/parser.py
@@ -9,9 +9,6 @@
 import os
 import sys
 
-# TODO set sys.pycache_prefix for >= python3.8
-sys.dont_write_bytecode = True
-
 # Make sure that mojom_parser.py can import mojom
 sys.path.insert(0, f'{os.path.dirname(__file__)}/mojo/public/tools/mojom')
 

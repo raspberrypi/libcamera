e334227dcc1584be03ca2220ea895da5725c39b8
Milan Zamazal <mzamazal@redhat.com>
2024-09-27 15:46:12 +0200

    libcamera: software_isp: Remove final dots in debayer.cpp docstrings
    The policy and the style checker require that \brief, \param and \return
    texts don't finish with a dot.  This needs to be fixed in debayer.cpp.
    Also leading spaces in a \return statement are removed from there.
    
    Signed-off-by: Milan Zamazal <mzamazal@redhat.com>
    Reviewed-by: Umang Jain <umang.jain@ideasonboard.com>
    Reviewed-by: Daniel Scally <dan.scally@ideasonboard.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- src/libcamera/software_isp/debayer.cpp
+++ src/libcamera/software_isp/debayer.cpp
@@ -58,47 +58,47 @@ Debayer::~Debayer()
 
 /**
  * \fn int Debayer::configure(const StreamConfiguration &inputCfg, const std::vector<std::reference_wrapper<StreamConfiguration>> &outputCfgs)
- * \brief Configure the debayer object according to the passed in parameters.
- * \param[in] inputCfg The input configuration.
- * \param[in] outputCfgs The output configurations.
+ * \brief Configure the debayer object according to the passed in parameters
+ * \param[in] inputCfg The input configuration
+ * \param[in] outputCfgs The output configurations
  *
- * \return 0 on success, a negative errno on failure.
+ * \return 0 on success, a negative errno on failure
  */
 
 /**
  * \fn Size Debayer::patternSize(PixelFormat inputFormat)
- * \brief Get the width and height at which the bayer pattern repeats.
- * \param[in] inputFormat The input format.
+ * \brief Get the width and height at which the bayer pattern repeats
+ * \param[in] inputFormat The input format
  *
  * Valid sizes are: 2x2, 4x2 or 4x4.
  *
- * \return Pattern size or an empty size for unsupported inputFormats.
+ * \return Pattern size or an empty size for unsupported inputFormats
  */
 
 /**
  * \fn std::vector<PixelFormat> Debayer::formats(PixelFormat inputFormat)
- * \brief Get the supported output formats.
- * \param[in] inputFormat The input format.
+ * \brief Get the supported output formats
+ * \param[in] inputFormat The input format
  *
- * \return All supported output formats or an empty vector if there are none.
+ * \return All supported output formats or an empty vector if there are none
  */
 
 /**
  * \fn std::tuple<unsigned int, unsigned int> Debayer::strideAndFrameSize(const PixelFormat &outputFormat, const Size &size)
- * \brief Get the stride and the frame size.
- * \param[in] outputFormat The output format.
- * \param[in] size The output size.
+ * \brief Get the stride and the frame size
+ * \param[in] outputFormat The output format
+ * \param[in] size The output size
  *
  * \return A tuple of the stride and the frame size, or a tuple with 0,0 if
- *    there is no valid output config.
+ * there is no valid output config
  */
 
 /**
  * \fn void Debayer::process(FrameBuffer *input, FrameBuffer *output, DebayerParams params)
- * \brief Process the bayer data into the requested format.
- * \param[in] input The input buffer.
- * \param[in] output The output buffer.
- * \param[in] params The parameters to be used in debayering.
+ * \brief Process the bayer data into the requested format
+ * \param[in] input The input buffer
+ * \param[in] output The output buffer
+ * \param[in] params The parameters to be used in debayering
  *
  * \note DebayerParams is passed by value deliberately so that a copy is passed
  * when this is run in another thread by invokeMethod().
@@ -106,21 +106,21 @@ Debayer::~Debayer()
 
 /**
  * \fn virtual SizeRange Debayer::sizes(PixelFormat inputFormat, const Size &inputSize)
- * \brief Get the supported output sizes for the given input format and size.
- * \param[in] inputFormat The input format.
- * \param[in] inputSize The input size.
+ * \brief Get the supported output sizes for the given input format and size
+ * \param[in] inputFormat The input format
+ * \param[in] inputSize The input size
  *
- * \return The valid size ranges or an empty range if there are none.
+ * \return The valid size ranges or an empty range if there are none
  */
 
 /**
  * \var Signal<FrameBuffer *> Debayer::inputBufferReady
- * \brief Signals when the input buffer is ready.
+ * \brief Signals when the input buffer is ready
  */
 
 /**
  * \var Signal<FrameBuffer *> Debayer::outputBufferReady
- * \brief Signals when the output buffer is ready.
+ * \brief Signals when the output buffer is ready
  */
 
 } /* namespace libcamera */

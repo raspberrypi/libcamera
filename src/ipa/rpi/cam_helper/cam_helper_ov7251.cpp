994588fb75838b46d5eb74b916d66f0b9fc49421
Dave Stevenson <dave.stevenson@raspberrypi.com>
2024-10-10 14:41:48 +0100

    ipa: rpi: Add tuning files for OV7251
    OV7251 is a mono VGA global shutter sensor that has a mainline
    driver and works with libcamera.
    Add the supporting files for it. The tuning is copied from OV9281.
    
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
    Reviewed-by: Naushir Patuck <naush@raspberrypi.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

---
+++ src/ipa/rpi/cam_helper/cam_helper_ov7251.cpp
@@ -0,0 +1,66 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (C) 2021, Raspberry Pi Ltd
+ *
+ * camera information for ov7251 sensor
+ */
+
+#include <assert.h>
+
+#include "cam_helper.h"
+
+using namespace RPiController;
+
+class CamHelperOv7251 : public CamHelper
+{
+public:
+	CamHelperOv7251();
+	uint32_t gainCode(double gain) const override;
+	double gain(uint32_t gainCode) const override;
+	void getDelays(int &exposureDelay, int &gainDelay,
+		       int &vblankDelay, int &hblankDelay) const override;
+
+private:
+	/*
+	 * Smallest difference between the frame length and integration time,
+	 * in units of lines.
+	 */
+	static constexpr int frameIntegrationDiff = 4;
+};
+
+/*
+ * OV7251 doesn't output metadata, so we have to use the "unicam parser" which
+ * works by counting frames.
+ */
+
+CamHelperOv7251::CamHelperOv7251()
+	: CamHelper({}, frameIntegrationDiff)
+{
+}
+
+uint32_t CamHelperOv7251::gainCode(double gain) const
+{
+	return static_cast<uint32_t>(gain * 16.0);
+}
+
+double CamHelperOv7251::gain(uint32_t gainCode) const
+{
+	return static_cast<double>(gainCode) / 16.0;
+}
+
+void CamHelperOv7251::getDelays(int &exposureDelay, int &gainDelay,
+				int &vblankDelay, int &hblankDelay) const
+{
+	/* The driver appears to behave as follows: */
+	exposureDelay = 2;
+	gainDelay = 2;
+	vblankDelay = 2;
+	hblankDelay = 2;
+}
+
+static CamHelper *create()
+{
+	return new CamHelperOv7251();
+}
+
+static RegisterCamHelper reg("ov7251", &create);

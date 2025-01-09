raspberrypi/libcamera

commit 39fe4ad96803f4dd97f034c3e4c05c55757bce11

utils: codegen: controls.py: Parse direction information
In preparation for adding support for querying direction information
from controls, parse the direction information from control ID
definitions. This can later be plugged in directly to the IPA code
generators simply by using ctrl.direction.

Signed-off-by: Paul Elder <paul.elder@ideasonboard.com>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Reviewed-by: Stefan Klug <stefan.klug@ideasonboard.com>

utils/codegen/controls.py
@@ -28,7 +28,7 @@ class ControlEnum(object):
 
 
 class Control(object):
-    def __init__(self, name, data, vendor):
+    def __init__(self, name, data, vendor, mode):
         self.__name = name
         self.__data = data
         self.__enum_values = None
@@ -59,6 +59,16 @@ class Control(object):
                 num_elems *= dim
 
             self.__size = num_elems
+
+        if mode == 'properties':
+            self.__direction = 'out'
+        else:
+            direction = self.__data.get('direction')
+            if direction is None:
+                raise RuntimeError(f'Control `{self.__name}` missing required field `{direction}`')
+            if direction not in ['in', 'out', 'inout']:
+                raise RuntimeError(f'Control `{self.__name}` direction `{direction}` is invalid; must be one of `in`, `out`, or `inout`')
+            self.__direction = direction
 
     @property
     def description(self):
@@ -110,6 +120,18 @@ class Control(object):
             return f"Span<const {typ}, {self.__size}>"
         else:
             return f"Span<const {typ}>"
+
+    @property
+    def direction(self):
+        in_flag = 'ControlId::Direction::In'
+        out_flag = 'ControlId::Direction::Out'
+
+        if self.__direction == 'inout':
+            return f'{in_flag} | {out_flag}'
+        if self.__direction == 'in':
+            return in_flag
+        if self.__direction == 'out':
+            return out_flag
 
     @property
     def element_type(self):

src/py/libcamera/gen-py-controls.py
@@ -83,7 +83,7 @@ def main(argv):
             vendors.append(vendor)
 
         for ctrl in data['controls']:
-            ctrl = Control(*ctrl.popitem(), vendor)
+            ctrl = Control(*ctrl.popitem(), vendor, args.mode)
             controls.append(extend_control(ctrl, args.mode))
 
     data = {

utils/codegen/gen-controls.py
@@ -71,7 +71,7 @@ def main(argv):
         ctrls = controls.setdefault(vendor, [])
 
         for i, ctrl in enumerate(data['controls']):
-            ctrl = Control(*ctrl.popitem(), vendor)
+            ctrl = Control(*ctrl.popitem(), vendor, args.mode)
             ctrls.append(extend_control(ctrl, i, ranges))
 
     # Sort the vendors by range numerical value

utils/codegen/gen-gst-controls.py
@@ -154,7 +154,7 @@ def main(argv):
         ctrls = controls.setdefault(vendor, [])
 
         for ctrl in data['controls']:
-            ctrl = Control(*ctrl.popitem(), vendor)
+            ctrl = Control(*ctrl.popitem(), vendor, mode='controls')
 
             if ctrl.name in exposed_controls:
                 ctrls.append(extend_control(ctrl))

commit 8d50577c0f6a4ff775b774e5803f7ec0928ff2c7

utils: codegen: controls.py: Fix missing direction error message
The error message for missing direction field prints the direction value
(usually 'None') instead of the name of the field 'direction'. Fix this.

Fixes: 39fe4ad96803 ("utils: codegen: controls.py: Parse direction information")
Signed-off-by: Paul Elder <paul.elder@ideasonboard.com>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

utils/codegen/controls.py
@@ -65,7 +65,7 @@ class Control(object):
         else:
             direction = self.__data.get('direction')
             if direction is None:
-                raise RuntimeError(f'Control `{self.__name}` missing required field `{direction}`')
+                raise RuntimeError(f'Control `{self.__name}` missing required field `direction`')
             if direction not in ['in', 'out', 'inout']:
                 raise RuntimeError(f'Control `{self.__name}` direction `{direction}` is invalid; must be one of `in`, `out`, or `inout`')
             self.__direction = direction
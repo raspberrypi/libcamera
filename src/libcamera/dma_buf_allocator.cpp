raspberrypi/libcamera

commit 545046a41e1767fa601c4e82608053f1912146af

DmaBufAllocator: Make DmaSyncer non-copyable
As DmaSyncer does sync start/end in the c'tor/d'tor, copying a DmaSyncer
instance would trigger sync end earlier than expected. This patch makes
it non-copyable to avoid the issue.

Fixes: 39482d59fe71 ("DmaBufAllocator: Add Dma Buffer synchronization function & helper class")
Signed-off-by: Harvey Yang <chenghaoyang@chromium.org>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/libcamera/dma_buf_allocator.cpp
@@ -311,6 +311,18 @@ DmaSyncer::DmaSyncer(SharedFD fd, SyncType type)
 	sync(DMA_BUF_SYNC_START);
 }
 
+/**
+ * \fn DmaSyncer::DmaSyncer(DmaSyncer &&other);
+ * \param[in] other The other instance
+ * \brief Enable move on class DmaSyncer
+ */
+
+/**
+ * \fn DmaSyncer::operator=(DmaSyncer &&other);
+ * \param[in] other The other instance
+ * \brief Enable move on class DmaSyncer
+ */
+
 DmaSyncer::~DmaSyncer()
 {
 	sync(DMA_BUF_SYNC_END);

include/libcamera/internal/dma_buf_allocator.h
@@ -60,9 +60,14 @@ public:
 
 	explicit DmaSyncer(SharedFD fd, SyncType type = SyncType::ReadWrite);
 
+	DmaSyncer(DmaSyncer &&other) = default;
+	DmaSyncer &operator=(DmaSyncer &&other) = default;
+
 	~DmaSyncer();
 
 private:
+	LIBCAMERA_DISABLE_COPY(DmaSyncer)
+
 	void sync(uint64_t step);
 
 	SharedFD fd_;

commit 4dad8ece72c0c1c6c9e0101f4c31937bd739d001

DmaBufAllocator: Avoid syncing with an invalid file descriptor
As DmaSyncer disables the copy c'tor, the move c'tor will be used
instead. This leaves some DmaSyncers with invalid SharedFDs. They should
avoid syncing with invalid file descriptors in the d'tor.

Fixes: 545046a41e17 ("DmaBufAllocator: Make DmaSyncer non-copyable")
Signed-off-by: Harvey Yang <chenghaoyang@chromium.org>
Tested-by: Milan Zamazal <mzamazal@redhat.com>
Reviewed-by: Milan Zamazal <mzamazal@redhat.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

src/libcamera/dma_buf_allocator.cpp
@@ -325,7 +325,12 @@ DmaSyncer::DmaSyncer(SharedFD fd, SyncType type)
 
 DmaSyncer::~DmaSyncer()
 {
-	sync(DMA_BUF_SYNC_END);
+	/*
+	 * DmaSyncer might be moved and left with an empty SharedFD.
+	 * Avoid syncing with an invalid file descriptor in this case.
+	 */
+	if (fd_.isValid())
+		sync(DMA_BUF_SYNC_END);
 }
 
 void DmaSyncer::sync(uint64_t step)
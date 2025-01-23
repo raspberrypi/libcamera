3acacb089dbe8aa26b8085591bad10e317f3de1a
Stefan Klug <stefan.klug@ideasonboard.com>
2025-01-23 12:40:52 +0100

    libipa: pwl: Add clear() function
    Sometimes it is necessary to clear a pwl. Add a function for that.
    
    Signed-off-by: Stefan Klug <stefan.klug@ideasonboard.com>
    Reviewed-by: Paul Elder <paul.elder@ideasonboard.com>
    Reviewed-by: Daniel Scally <dan.scally@ideasonboard.com>

--- src/ipa/libipa/pwl.h
+++ src/ipa/libipa/pwl.h
@@ -49,6 +49,7 @@ public:
 	void append(double x, double y, double eps = 1e-6);
 
 	bool empty() const { return points_.empty(); }
+	void clear() { points_.clear(); }
 	size_t size() const { return points_.size(); }
 
 	Interval domain() const;

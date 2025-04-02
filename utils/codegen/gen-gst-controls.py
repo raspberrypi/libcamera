raspberrypi/libcamera

commit 187f2d537be5a4ad2c73649653cf925b74de61fe

gstreamer: Generate the new AEGC controls
Since AeEnable will be replaced with ExposureTimeMode and
AnalogueGainMode so that the two can be set between auto/manual
independently, update the gstreamer control ids generation to conform
with this.

Signed-off-by: Paul Elder <paul.elder@ideasonboard.com>
Reviewed-by: Nicolas Nicolas <nicolas.dufresne@collabora.com>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

utils/codegen/gen-gst-controls.py
@@ -19,8 +19,9 @@ from controls import Control
 
 
 exposed_controls = [
-    'AeEnable', 'AeMeteringMode', 'AeConstraintMode', 'AeExposureMode',
-    'ExposureValue', 'ExposureTime', 'AnalogueGain', 'AeFlickerPeriod',
+    'AeMeteringMode', 'AeConstraintMode', 'AeExposureMode',
+    'ExposureValue', 'ExposureTime', 'ExposureTimeMode',
+    'AnalogueGain', 'AnalogueGainMode', 'AeFlickerPeriod',
     'Brightness', 'Contrast', 'AwbEnable', 'AwbMode', 'ColourGains',
     'Saturation', 'Sharpness', 'ColourCorrectionMatrix', 'ScalerCrop',
     'DigitalGain', 'AfMode', 'AfRange', 'AfSpeed', 'AfMetering', 'AfWindows',

commit 61d93434f5830cc9b1e057db2834fc2a64622f21

gstreamer: Restore `AeEnable` control
Commit "gstreamer: Generate the new AEGC controls" removed the
`AeEnable` control from gen-gst-controls.py. However, the patch
set it was part of did not end up removing the `AeEnable`
control after all. So restore it for gstreamer users.

See 85cb179f289d29 ("controls: Redefine AeEnable").

Fixes: 187f2d537be5a4 ("gstreamer: Generate the new AEGC controls")
Signed-off-by: Barnabás Pőcze <barnabas.pocze@ideasonboard.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

utils/codegen/gen-gst-controls.py
@@ -19,7 +19,7 @@ from controls import Control
 
 
 exposed_controls = [
-    'AeMeteringMode', 'AeConstraintMode', 'AeExposureMode',
+    'AeEnable', 'AeMeteringMode', 'AeConstraintMode', 'AeExposureMode',
     'ExposureValue', 'ExposureTime', 'ExposureTimeMode',
     'AnalogueGain', 'AnalogueGainMode', 'AeFlickerPeriod',
     'Brightness', 'Contrast', 'AwbEnable', 'AwbMode', 'ColourGains',
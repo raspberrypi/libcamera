2f7c474479ed8b4643ab3d29b373aa03c883e5f9
Stefan Klug <stefan.klug@ideasonboard.com>
2025-09-19 11:40:34 +0200

    tuning: rksip1: Add a static WideDynamicRange entry
    Add a static WideDynamicRange entry that gets added by default.
    
    Signed-off-by: Stefan Klug <stefan.klug@ideasonboard.com>
    Reviewed-by: Paul Elder <paul.elder@ideasonboard.com>
    Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- utils/tuning/rkisp1.py
+++ utils/tuning/rkisp1.py
@@ -48,16 +48,18 @@ lsc = LSCRkISP1(debug=[lt.Debug.Plot],
                 # values.  This can also be a custom function.
                 smoothing_function=lt.smoothing.MedianBlur(3),)
 lux = LuxRkISP1(debug=[lt.Debug.Plot])
+wdr = StaticModule('WideDynamicRange',
+                   {'ExposureConstraint': {'MaxBrightPixels': 0.02, 'yTarget': 0.95}})
 
 tuner = lt.Tuner('RkISP1')
-tuner.add([agc, awb, blc, ccm, color_processing, filter, gamma_out, lsc, lux, compress])
+tuner.add([agc, awb, blc, ccm, color_processing, filter, gamma_out, lsc, lux, wdr, compress])
 tuner.set_input_parser(YamlParser())
 tuner.set_output_formatter(YamlOutput())
 
 # Bayesian AWB uses the lux value, so insert the lux algorithm before AWB.
 # Compress is parameterized by others, so add it at the end.
 tuner.set_output_order([agc, lux, awb, blc, ccm, color_processing,
-                        filter, gamma_out, lsc, compress])
+                        filter, gamma_out, lsc, wdr, compress])
 
 if __name__ == '__main__':
     sys.exit(tuner.run(sys.argv))
